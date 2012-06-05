a165a451617a970d7a119c9f44a0933bf25e32ee
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-06-05 07:22:25 +0200

    added attribute types select and translatedselect. Added base class MetaModelAttributeHybrid for hybrid attributes that are of complex nature but also need to store a column in the metamodel table.

---
+++ src/system/modules/metamodels/MetaModelAttributeHybrid.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  CyberSpectrum
+ * @license    private
+ * @filesource
+ */
+if (!defined('TL_ROOT'))
+{
+	die('You cannot access this file directly!');
+}
+
+/**
+ * Base implementation for hybrid attributes.
+ * Hybrid attributes are attributes that have a column in the underlying metamodel table but reference auxiliary
+ * data.
+ * 
+ * @package	   MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ */
+abstract class MetaModelAttributeHybrid extends MetaModelAttributeSimple implements IMetaModelAttributeComplex
+{
+
+	/////////////////////////////////////////////////////////////////
+	// interface IMetaModelAttribute
+	/////////////////////////////////////////////////////////////////
+
+	/////////////////////////////////////////////////////////////////
+	// interface IMetaModelAttributeComplex
+	/////////////////////////////////////////////////////////////////
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function parseFilterUrl($arrUrlParams)
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIdsFromFilter($arrFilter)
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function sortIds($arrIds, $strDirection)
+	{
+		return $arrIds;
+	}
+
+}
+
+?>
