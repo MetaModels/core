b7f26fe2255fe9d18b43ef9a5b5656c98258d05e
Carolina M. Koehn <office@kikmedia.de>
2012-09-10 15:15:07 +0200

    Minor fixes.

--- src/system/modules/metamodels/languages/de/tl_metamodel_rendersettings.php
+++ src/system/modules/metamodels/languages/de/tl_metamodel_rendersettings.php
@@ -24,7 +24,7 @@ if (!defined('TL_ROOT'))
  */
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['name']                 = array('Name', 'Einstellungsname.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['tstamp']               = array('Änderungsdatum', 'Datum und Zeit der letzten Änderung.');
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['isdefault']            = array('Standard', 'Bestimmt, ob diese Einstellung als Standardvorgabe bei Eltern-Kind-Beziehungen im MetaModel benutzt werden soll, wenn keine anderen Voreinstellungen getroffen wurden.');
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['isdefault']            = array('Standard', 'Bestimmt, ob diese Einstellung als Standardvorgabe bei Eltern-Kind-Beziehungen im MetaModel benutzt werden soll, wenn in einem Modul/Contentelement/etc. keines explizit ausgewählt wurde.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['template']             = array('Template', 'Das Template festlegen, das für die Darstellung der Datensätze benutzt werden soll.');
 
 /**
