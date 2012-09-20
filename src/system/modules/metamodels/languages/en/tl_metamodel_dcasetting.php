d0b0bc00d413c29552cbf9469e67648e8ad0dc1f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-09-20 12:52:30 +0200

    moved styling information of palette in list overview to css.

--- src/system/modules/metamodels/languages/en/tl_metamodel_dcasetting.php
+++ src/system/modules/metamodels/languages/en/tl_metamodel_dcasetting.php
@@ -42,6 +42,6 @@ $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['show']            = array('Setti
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['addall']          = array('Add all', 'Add all attributes to palette');
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['row']             = '%s <strong>%s</strong> <em>[%s]</em>';
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legend_row']      = '<div style="color: #8AB858; padding-left: 18px; padding-right: 3px; background: url(system/themes/default/images/palOpen.gif) left center no-repeat; cursor: pointer;">%stest....</div>';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legend_row']      = '<div class="dca_palette">%stest....</div>';
 
 ?>
