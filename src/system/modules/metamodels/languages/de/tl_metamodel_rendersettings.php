e4ba8a6f128f0d1513ed4d1de89dd3097cd1c9df
Hannes <github@gmx.net>
2012-11-24 14:50:23 +0100

    Update src/system/modules/metamodels/languages/de/tl_metamodel_rendersettings.php

--- src/system/modules/metamodels/languages/de/tl_metamodel_rendersettings.php
+++ src/system/modules/metamodels/languages/de/tl_metamodel_rendersettings.php
@@ -32,9 +32,9 @@ $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['expert_legend']        = 'Ex
  */
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['name']                 = array('Name', 'Einstellungsname.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['tstamp']               = array('Änderungsdatum', 'Datum und Zeit der letzten Änderung.');
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['isdefault']            = array('Standard', 'Bestimmt, ob diese Einstellung als Standardvorgabe bei Eltern-Kind-Beziehungen im MetaModel benutzt werden soll, wenn in einem Modul/Contentelement/etc. keines explizit ausgewählt wurde.');
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['isdefault']            = array('Standard', 'Bestimmt, ob diese Einstellung als Standardvorgabe bei Eltern-Kind-Beziehungen im MetaModel benutzt werden soll, wenn in einem Modul oder Inhaltselement keines explizit ausgewählt wurde.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['template']             = array('Template', 'Das Template festlegen, das für die Darstellung der Datensätze benutzt werden soll.');
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['jumpTo']               = array('Zielseite', 'Die Seite (URL) festlegen, die für die Darstellung der Details benutzt wird. Detaillierte URL-Parameter werden von der jeweilig benutzten Filtereinstellung generiert.');
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['jumpTo']               = array('Zielseite', 'Die Seite (URL) festlegen, die für die Darstellung der Details benutzt wird. Detaillierte URL-Parameter werden von der jeweiligen Filtereinstellung generiert.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['jumpTo']['allLanguages'] = 'Alle Sprachen';
 
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['mode']                 = array('Sortiermodus', 'Den Sortiermodus einstellen, der in der Datensatzdarstellung benutzt werden soll.');
@@ -42,8 +42,8 @@ $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['flag']                 = arr
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['panelLayout']          = array('Panel-Layout', 'Für Zwischenräume die Optionen mit einem Komma (für Leerzeichen) und Semikolon (für eine neue Zeile) eingeben. Beispiel: sort,filter;search,limit.');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['panelpicker']          = 'Panelpicker';
 
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['fields']               = array('Feldeinstellungen', 'Die Header-Felder konfigurieren, um eine besser Bedienbarkeit zu erreichen.');
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['field_attribute']      = 'Attribut-Name';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['fields']               = array('Feldeinstellungen', 'Die Header-Felder konfigurieren, um eine bessere Bedienbarkeit zu erreichen.');
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['field_attribute']      = 'Attributname';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['field_filterable']     = 'Filterbar';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['field_sortable']       = 'Sortierbar';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['field_searchable']     = 'Durchsuchbar';
@@ -55,7 +55,7 @@ $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingmode']['1']     = 'Da
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingmode']['2']     = 'Datensätze werden nach einem wechselbaren Feld sortiert';
 
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['1']     = 'Nach erstem Buchstaben aufsteigend sortieren';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['2']     = 'Nach erstem Buchstaben abfsteigend sortieren';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['2']     = 'Nach erstem Buchstaben absteigend sortieren';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['3']     = 'Nach den ersten beiden Buchstaben aufsteigend sortieren';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['4']     = 'Nach den ersten beiden Buchstaben absteigend sortieren';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['sortingflag']['5']     = 'Nach Kalendertag aufsteigend sortieren';
@@ -76,6 +76,6 @@ $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['edit']                 = arr
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['copy']                 = array('Einstellungsdefinition kopieren', 'Die Einstellungsdefinition ID %s kopieren');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['delete']               = array('Einstellungsdefinition löschen', 'Die Einstellungsdefinition ID %s löschen');
 $GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['show']                 = array('Filterdetails', 'Die Details der Einstellungsdefinition ID %s anzeigen');
-$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['settings']             = array('Attributeinstellungen definieren', 'Die Attributeinstllungen für ID %s definieren');
+$GLOBALS['TL_LANG']['tl_metamodel_rendersettings']['settings']             = array('Attributeinstellungen definieren', 'Die Attributeinstellungen für ID %s definieren');
 
 ?>
