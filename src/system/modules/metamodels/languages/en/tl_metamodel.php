160051995cd5a1e464079f6d6147a7ad887aa797
Hannes <github@gmx.net>
2012-11-24 15:48:13 +0100

    Update src/system/modules/metamodels/languages/en/tl_metamodel.php

--- src/system/modules/metamodels/languages/en/tl_metamodel.php
+++ src/system/modules/metamodels/languages/en/tl_metamodel.php
@@ -23,10 +23,10 @@ $GLOBALS['TL_LANG']['tl_metamodel']['tableName']            = array('Table name'
 
 $GLOBALS['TL_LANG']['tl_metamodel']['mode']                 = array('Parent list mode', 'Mode to use for parent/child relationship.');
 
-$GLOBALS['TL_LANG']['tl_metamodel']['translated']           = array('Translation', 'Check if this MetaModel shall support translation/multilanguage.');
+$GLOBALS['TL_LANG']['tl_metamodel']['translated']           = array('Translation', 'Check if this MetaModel shall support translation/multilingualism.');
 $GLOBALS['TL_LANG']['tl_metamodel']['languages']            = array('Languages to provide for translation', 'Specify all languages that shall be available for translation.');
-$GLOBALS['TL_LANG']['tl_metamodel']['languages_langcode']   = array('Language', 'Select the languages you want to provide');
-$GLOBALS['TL_LANG']['tl_metamodel']['languages_isfallback'] = array('Fallback language', 'Check the language that shall be used as fallback');
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_langcode']   = array('Language', 'Select the languages you want to provide.');
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_isfallback'] = array('Fallback language', 'Check the language that shall be used as fallback.');
 
 $GLOBALS['TL_LANG']['tl_metamodel']['varsupport']           = array('Variant support', 'Check if this MetaModel shall support variants of items.');
 
@@ -39,10 +39,10 @@ $GLOBALS['TL_LANG']['tl_metamodel']['advanced_legend']      = 'Advanced settings
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_metamodel']['new']                  = array('New MetaModel', 'Create new MetaModel.');
+$GLOBALS['TL_LANG']['tl_metamodel']['new']                  = array('New MetaModel', 'Create a new MetaModel.');
 
 $GLOBALS['TL_LANG']['tl_metamodel']['edit']                 = array('Manage items', 'Manage items of MetaModel ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel']['copy']                 = array('Copy MetaModel definiton', 'Copy definition of MetaModel ID %s');
+$GLOBALS['TL_LANG']['tl_metamodel']['copy']                 = array('Copy MetaModel definition', 'Copy definition of MetaModel ID %s');
 $GLOBALS['TL_LANG']['tl_metamodel']['delete']               = array('Delete MetaModel', 'Delete MetaModel ID %s');
 $GLOBALS['TL_LANG']['tl_metamodel']['show']                 = array('MetaModel details', 'Show details of MetaModel ID %s');
 $GLOBALS['TL_LANG']['tl_metamodel']['editheader']           = array('Edit MetaModel', 'Edit the MetaModel');
