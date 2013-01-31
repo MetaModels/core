422bf054a26849422d6687f82bcbe72a4d1f73a3
Andreas Isaak <andy.jared@googlemail.com>
2013-01-31 12:52:32 +0100

    Supporting syncCto

---
+++ src/system/modules/metamodels/languages/de/tl_syncCto_database.php
@@ -0,0 +1,22 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2013 
+ * @package    syncCto
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel']                     = 'MetaModels';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_attribute']           = 'MetaModels Attribute';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dca']                 = 'MetaModels Paletten';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dca_combine']         = 'MetaModels Workflow';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dcasetting']          = 'MetaModels Palettenkonfiguration';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_filter']              = 'MetaModels Filter';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_filtersetting']       = 'MetaModels Filtereinstellung';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_rendersetting']       = 'MetaModels Vorlageeinstellungen';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_rendersettings']      = 'MetaModels Vorlagen';
+
+?>
---
+++ src/system/modules/metamodels/languages/en/tl_syncCto_database.php
@@ -0,0 +1,22 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2013 
+ * @package    syncCto
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel']                     = 'MetaModels';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_attribute']           = 'MetaModels attribute';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dca']                 = 'MetaModels palettes';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dca_combine']         = 'MetaModels workflow';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_dcasetting']          = 'MetaModels palette setting';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_filter']              = 'MetaModels filter';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_filtersetting']       = 'MetaModels filter setting';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_rendersetting']       = 'MetaModels render setting';
+$GLOBALS['TL_LANG']['tl_syncCto_database']['tl_metamodel_rendersettings']      = 'MetaModels render settings';
+
+?>
