98fb31e19bf8f77ff6d64b62827da2bf1c675939
Andreas Isaak <andy.jared@googlemail.com>
2013-03-05 12:57:33 +0100

    Add new metamodels_src.js

--- src/system/modules/metamodels/html/metamodels.js
+++ src/system/modules/metamodels/html/metamodels.js
@@ -4,7 +4,7 @@
  * The Front-End modules allow you to build powerful listing and filtering of the
  * data in each collection.
  *
- * @package	   MetaModels
+ * @package    MetaModels
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @copyright  The MetaModels team.
@@ -12,82 +12,4 @@
  * @filesource
  */
 
-/**
- * Developers need to apply a snippet like this for each 
- * class they want to use:
- *
- * MetaModelsFE.addClassHook('someclass', function(element, key){ 
- *    alert("the class " + key + " has been applied to " + element)
- * });
- */
-
-var MetaModels = new Class({
-
-    Implements: Options,
-
-    /**
-     * Options
-     */
-    options: {},
-
-    /**
-     * List for additions functions
-     */
-    classhooks: {},
-
-    /**
-     * Init
-     */
-    initialize: function(options) {
-        this.setOptions(options);
-
-        this.addClassHook('submitonchange', this.applySubmitOnChange);
-        this.addClassHook('submitonclick', this.applySubmitOnClick);
-    },
-
-    /**
-     * Hook functions, for adding/remove new functions
-     */
-    addClassHook: function(name, callback) {
-        this.classhooks[name] = callback;
-    },
-
-    removeClassHook: function(name) {
-        delete(this.classhooks[name]);
-    },
-
-    /**
-     * apply all class handlers to their elements.
-     */
-    applyClassHooks: function() {
-        Object.each(this.classhooks, function(value, key) {
-            $$('.' + key).each(function(el, key) {
-                value(el);
-            });
-        });
-    },
-
-    /**
-     * Local helper for form elements to submit the form.
-     */
-
-    applySubmitOnChange: function(el) {
-        $(el).addEvent('change', function() {
-            $(el).getParent('form').submit()
-        });
-    },
-
-    applySubmitOnClick: function(el) {
-        $(el).addEvent('click', function() {
-            $(el).getParent('form').submit()
-        });
-    },
-
-});
-
-var MetaModelsFE = new MetaModels();
-
-// apply all class hooks on domready
-window.addEvent('domready', function() {
-    MetaModelsFE.applyClassHooks();
-});
+var MetaModels=new Class({Implements:Options,options:{},classhooks:{},initialize:function(a){this.setOptions(a);this.addClassHook("submitonchange",this.applySubmitOnChange);this.addClassHook("submitonclick",this.applySubmitOnClick)},addClassHook:function(a,b){this.classhooks[a]=b},removeClassHook:function(a){delete (this.classhooks[a])},applyClassHooks:function(){Object.each(this.classhooks,function(b,a){$$("."+a).each(function(d,c){b(d)})})},applySubmitOnChange:function(a){$(a).addEvent("change",function(){$(a).getParent("form").submit()})},applySubmitOnClick:function(a){$(a).addEvent("click",function(){$(a).getParent("form").submit()})},});var MetaModelsFE=new MetaModels();window.addEvent("domready",function(){MetaModelsFE.applyClassHooks()});
