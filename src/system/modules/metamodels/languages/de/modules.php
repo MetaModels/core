5a815dba9166e891f2591220217c44ff61c31fce
Mini Model <minimodel@metamodel.me>
2013-05-16 12:12:41 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/de/modules.php
+++ src/system/modules/metamodels/languages/de/modules.php
@@ -8,14 +8,14 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T23:07:50+02:00
+ * last-updated: 2013-05-15T08:12:18+02:00
  */
 
 $GLOBALS['TL_LANG']['FMD']['metamodel_list']['0']              = 'MetaModel-Liste';
 $GLOBALS['TL_LANG']['FMD']['metamodel_list']['1']              = 'Fügt eine Liste aus MetaModel-Datensätzen dem Artikel hinzu.';
 $GLOBALS['TL_LANG']['FMD']['metamodels']['0']                  = 'MetaModels';
 $GLOBALS['TL_LANG']['FMD']['metamodels']['1']                  = 'Die MetaModels-Erweiterung ermöglicht es, eigene Datenmodelle zu erstellen';
-$GLOBALS['TL_LANG']['FMD']['metamodels_frontendclearall']['0'] = 'Zurücksetzen';
+$GLOBALS['TL_LANG']['FMD']['metamodels_frontendclearall']['0'] = 'MetaModel-Filterreset';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendclearall']['1'] = 'Fügt eine Option zum Zurücksetzen aller Frontend-Filter hinzu.';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['0']   = 'MetaModel-Frontendfilter';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['1']   = 'Fügt einen Frontend-Filter für ein MetaModel hinzu';
