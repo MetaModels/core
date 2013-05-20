e887ff5809731093550e824fe3d8c7626d772652
Mini Model <minimodel@metamodel.me>
2013-05-20 20:12:28 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/el/tl_metamodel_dcasetting.php
+++ src/system/modules/metamodels/languages/el/tl_metamodel_dcasetting.php
@@ -8,27 +8,54 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:11:20+02:00
+ * last-updated: 2013-05-13T23:07:50+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['addall']['0']           = 'Προσθηκη ολων';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['advanced_legend']       = 'Προσαρμογη';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['allowHtml']['0']        = 'Κανενα html περιεχόμενο. ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['allowHtml']['1']        = 'Αν επιλέξετε αυτό, τοHTML περιεχόμενο θα ειναι διατηρημένο. ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['attr_id']['0']          = ' Χαρακτηριστικο';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['attr_id']['1']          = 'Χαρακτηριστικό που η ρύθμιση αυτή το αφορά.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['chosen']['0']           = 'Επιλογη';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['chosen']['1']           = 'Ενεργοποίηση επιλογης γραφικων widget.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['cols']['0']             = 'Στήλη ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['cols']['1']             = 'Ποσό των στηλών που θα χρησιμοποιηθεί για LONGTEXT / πίνακα widget ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['config_legend']         = 'Διαμόρφωση ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['copy']['0']             = 'Αντιγράφη ορισμού ρύθμισης ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['copy']['1']             = 'Αντιγραφή ρύθμισης ID%s ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['dcatype']['0']          = 'Τυπος';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['dcatype']['1']          = 'Επιλεξτε τον τυπο χαρακτηριστικόυ.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['dcatypes']['attribute'] = ' Χαρακτηριστικο';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['decodeEntities']['0']   = 'Αποκωδικοποίηση HTML οντότητων. ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['decodeEntities']['1']   = 'Επιλέξτε αυτό, εάν θέλετε η HTML οντότητες να αποκωδικοποιηθουν. ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['delete']['0']           = 'Διαγραφή ρύθμισης ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['delete']['1']           = 'Διαγραφή ρύθμισης ID %s';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['edit']['0']             = 'Επεξεργασια ρυθμισης';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['edit']['1']             = 'Επεξεργασια του ID %s ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['filterable']['0']       = 'Φιλτραρίσμα';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['flag']['0']             = 'Ταξινόμηση παράκαμψης σημαίας ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['flag']['1']             = 'Αν θέλετε να παρακάμψετε την παγκόσμια σημαία διαλογής από την παλέτα για αυτό το χαρακτηριστικό, παρακαλούμε επιλέξτε εδώ.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legendhide']['0']       = 'Απόκρυψη υπομνηματος';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legendhide']['1']       = 'Αποκρύψη υπόμνηματος για την προεπιλογή.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legendtitle']['0']      = 'Τιτλος υπομνηματος';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['legendtitle']['1']      = 'Εδώ μπορείτε να εισαγετε εναν τιτλο υπομνηματος.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['mandatory']['0']        = 'Υποχρεωση';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['mandatory']['1']        = 'Ελέγξτε αν αυτό το χαρακτηριστικό είναι υποχρεωτικο.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['name_langcode']         = 'Γλωσσα ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['name_value']            = 'Τιτλος υπομνηματος';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['new']['0']              = 'Νεος τυπος';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['new']['1']              = 'Δημιουργια νεας ρυθμισης';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['preserveTags']['0']     = 'Καμμια κωδικοποιηση των html tags. ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['preserveTags']['1']     = 'ΑΝ το επιλεξετε αυτο, τα HTML tags δεν θα κωδικοποιηθουν.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['rows']['0']             = 'Σειρές';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['rows']['1']             = 'Ποσό των σειρων που θα χρησιμοποιηθουν για τον LONGTEXT/πίνακα widget.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['rte']['0']              = 'Ενεργοποίηση Richtext editor για το θέμα αυτό';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['rte']['1']              = 'Επιλέξτε την πλούσια διαμόρφωση κειμένου που πρέπει να χρησιμοποιείται σε αυτόν τον τομέα (εάν υπάρχει). ';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['searchable']['0']       = 'Αναζητησιμο';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['show']['1']             = 'Δείτε λεπτομέρειες σχετικά με τη ρύθμιση ID%s';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['sortable']['0']         = 'Ανακαταταξη';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['stylepicker']           = 'Style picker';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['template']['0']         = 'Προσαρμοσμένο πρότυπο που θα χρησιμοποιηθεί για την παραγωγή ';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['template']['1']         = 'Επιλέξτε το πρότυπο που πρέπει να χρησιμοποιηθεί για την επιλεγμένη ιδιότητα. Ισχύει για αρχεία προτύπων αρχίζουν με "mm_&lt;type&gt;" όπου το όνομα του τύπου τίθεται για &lt;type&gt;';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['title_legend']          = 'Τυπος';
