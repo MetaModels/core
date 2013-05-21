d0c40fd0799354333a1d9762150334d42d4133fd
Mini Model <minimodel@metamodel.me>
2013-05-21 08:12:22 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/el/tl_metamodel.php
+++ src/system/modules/metamodels/languages/el/tl_metamodel.php
@@ -8,12 +8,55 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:11:20+02:00
+ * last-updated: 2013-05-13T23:07:50+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel']['advanced_legend']         = 'Προσαρμοσμενες ρυθμισεις';
-$GLOBALS['TL_LANG']['tl_metamodel']['editheader']['0']         = 'Επεξεργασια του MetaModel';
-$GLOBALS['TL_LANG']['tl_metamodel']['languages_langcode']['0'] = 'Γλωσσα ';
-$GLOBALS['TL_LANG']['tl_metamodel']['name']['0']               = 'Ονομα';
-$GLOBALS['TL_LANG']['tl_metamodel']['tstamp']['0']             = 'Επεξεργασια ημερομηνιας';
+$GLOBALS['TL_LANG']['tl_metamodel']['advanced_legend']           = 'Προσαρμοσμενες ρυθμισεις';
+$GLOBALS['TL_LANG']['tl_metamodel']['copy']['0']                 = 'Αντιγραφη του MetaModel ορισμου';
+$GLOBALS['TL_LANG']['tl_metamodel']['copy']['1']                 = 'Αντιγραφη ορισμου του MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['cut']['0']                  = 'Μετακινηση του MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel']['cut']['1']                  = 'Ορίστε τη σειρά των μεταμοντέλων σας';
+$GLOBALS['TL_LANG']['tl_metamodel']['dca']['0']                  = 'Ορισμος οθόνης εισαγωγής';
+$GLOBALS['TL_LANG']['tl_metamodel']['dca']['1']                  = 'Ορισμος οθόνης εισαγωγής για το MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['dca_combine']['0']          = 'Ορίσμος εισόδου/εξόδου συνδυασμων ';
+$GLOBALS['TL_LANG']['tl_metamodel']['dca_combine']['1']          = 'Ορίσμος εισόδου/εξόδου συνδυασμων για το MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['delete']['0']               = 'Διαγραφη του MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel']['delete']['1']               = 'Διαγραφη του MetaModel ID %s ';
+$GLOBALS['TL_LANG']['tl_metamodel']['edit']['0']                 = 'Διαχειριση στοιχειων';
+$GLOBALS['TL_LANG']['tl_metamodel']['edit']['1']                 = 'Διαχειριση στοιχειων του MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['editheader']['0']           = 'Επεξεργασια του MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel']['editheader']['1']           = 'Επεξεργασια του MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel']['fields']['0']               = 'Ορίσμος χαρακτηριστικων ';
+$GLOBALS['TL_LANG']['tl_metamodel']['fields']['1']               = 'ΔΙαχειριση χαρακτηριστικων του MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['filter']['0']               = 'Ορισμος φιλτρων';
+$GLOBALS['TL_LANG']['tl_metamodel']['filter']['1']               = 'Ορισμος φιλτρων του MetaModel ID %s ';
+$GLOBALS['TL_LANG']['tl_metamodel']['itemFormat']                = '%s %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['itemPlural']                = 'στοιχεια';
+$GLOBALS['TL_LANG']['tl_metamodel']['itemSingle']                = 'στοιχειο';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages']['0']            = ' 	
+Γλώσσες για την παροχή μετάφρασης ';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages']['1']            = 'Καθορίστε όλες τις γλώσσες που είναι διαθέσιμες για μετάφραση.';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_isfallback']['0'] = 'Αναπληρωματική γλώσσα';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_isfallback']['1'] = 'Ελέγξτε τη γλώσσα που πρέπει να χρησιμοποιείται ως εφεδρική.';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_langcode']['0']   = 'Γλωσσα ';
+$GLOBALS['TL_LANG']['tl_metamodel']['languages_langcode']['1']   = 'Επιλέξτε τις γλώσσες που θέλετε να παρέχθουν.';
+$GLOBALS['TL_LANG']['tl_metamodel']['mode']['0']                 = 'Μητρική λίστα λειτουργίας';
+$GLOBALS['TL_LANG']['tl_metamodel']['mode']['1']                 = 'Λειτουργία που θα χρησιμοποιηθεί για τη σχέση γονέα/παιδιού.';
+$GLOBALS['TL_LANG']['tl_metamodel']['name']['0']                 = 'Ονομα';
+$GLOBALS['TL_LANG']['tl_metamodel']['name']['1']                 = 'MetaModel ονομα.';
+$GLOBALS['TL_LANG']['tl_metamodel']['new']['0']                  = 'Νεο MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel']['new']['1']                  = 'Δημιουργια ενος νεου MetaModel.';
+$GLOBALS['TL_LANG']['tl_metamodel']['rendersettings']['0']       = 'Ορισμος ρυθμισεων αποτυπωσης';
+$GLOBALS['TL_LANG']['tl_metamodel']['rendersettings']['1']       = 'Ορίσμος των ρυθμίσεων για το MetaModel ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel']['show']['0']                 = 'MetaModel λεπτομερειες';
+$GLOBALS['TL_LANG']['tl_metamodel']['show']['1']                 = 'Εμφανιση λεπτομερειων του MetaModel ID %s ';
+$GLOBALS['TL_LANG']['tl_metamodel']['tableName']['0']            = 'Ονομα ταμπελας ';
+$GLOBALS['TL_LANG']['tl_metamodel']['tableName']['1']            = 'Το όνομα της ταμπελας βάσης δεδομένων για την αποθήκευση αντικειμένων στο. ';
+$GLOBALS['TL_LANG']['tl_metamodel']['title_legend']              = 'Ονομα και ταμπελα';
+$GLOBALS['TL_LANG']['tl_metamodel']['translated']['0']           = 'Μεταφραση';
+$GLOBALS['TL_LANG']['tl_metamodel']['translated']['1']           = 'Ελέγξτε αν το MetaModel υποστηρίζει μετάφραση/πολυγλωσσία. ';
+$GLOBALS['TL_LANG']['tl_metamodel']['translated_legend']         = 'Μεταφραση';
+$GLOBALS['TL_LANG']['tl_metamodel']['tstamp']['0']               = 'Επεξεργασια ημερομηνιας';
+$GLOBALS['TL_LANG']['tl_metamodel']['tstamp']['1']               = 'Ημερομηνία και ώρα της τελευταίας αναθεώρησης.';
+$GLOBALS['TL_LANG']['tl_metamodel']['varsupport']['0']           = 'Υποστήριξη Variant';
+$GLOBALS['TL_LANG']['tl_metamodel']['varsupport']['1']           = 'Ελέγξτε αν το MetaModel θα στηρίξει παραλλαγές των αντικειμένων.';
