7c04a72061d5d7ae8940e153220090aaa570f41d
Stefan Heimes <stefan_heimes@hotmail.com>
2013-05-28 21:59:16 +0200

    Add an API for event handling. It is possible to add/remove all events from an object. At the moment only for jQuery.

--- src/system/modules/metamodels/html/metamodels_src.js
+++ src/system/modules/metamodels/html/metamodels_src.js
@@ -7,6 +7,7 @@
  * @package    MetaModels
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <cms@men-at-work.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
@@ -19,75 +20,250 @@
  * MetaModelsFE.addClassHook('someclass', function(element, key){ 
  *    alert("the class " + key + " has been applied to " + element)
  * });
+ * 
+ * jQuery.MetaModels.addClassHook('submitonclick', function(el, helper) {
+ *           // Remove old events
+ *           helper.unbindEvent({
+ *               object: el,
+ *               type: 'click'
+ *           });
+ *           
+ *           // Add new event
+ *           helper.bindEvent({
+ *               object: el,
+ *               type: 'click',
+ *               func: function(event) {
+ *                   // Stop event
+ *                   event.preventDefault();
+ *                   event.stopPropagation();
+ *                   
+ *                   // Do something
+ *                   
+ *                   return false;
+ *               }
+ *           });
+ *       }); 
  */
 
-var MetaModels = new Class({
+// Check if we have mootools
+if (typeof mootools !== 'undefined') {
 
-	Implements: Options,
+    (function($) {
+        var MetaModels = new Class({
 
-	/**
-	 * Options
-	 */
-	options: {},
+            Implements: Options,
 
-	/**
-	 * List for additions functions
-	 */
-	classhooks: {},
+            /**
+             * Options
+             */
+            options: {},
 
-	/**
-	 * Init
-	 */
-	initialize: function(options) {
-		this.setOptions(options);
+            /**
+             * List for additions functions
+             */
+            classhooks: {},
 
-		this.addClassHook('submitonchange', this.applySubmitOnChange);
-		this.addClassHook('submitonclick', this.applySubmitOnClick);
-	},
+            /**
+             * Init
+             */
+            initialize: function(options) {
+                this.setOptions(options);
 
-	/**
-	 * Hook functions, for adding/remove new functions
-	 */
-	addClassHook: function(name, callback) {
-		this.classhooks[name] = callback;
-	},
+                this.addClassHook('submitonchange', this.applySubmitOnChange);
+                this.addClassHook('submitonclick', this.applySubmitOnClick);
+            },
 
-	removeClassHook: function(name) {
-		delete(this.classhooks[name]);
-	},
+            /**
+             * Hook functions, for adding/remove new functions
+             */
+            addClassHook: function(name, callback) {
+                this.classhooks[name] = callback;
+            },
 
-	/**
-	 * apply all class handlers to their elements.
-	 */
-	applyClassHooks: function() {
-		Object.each(this.classhooks, function(value, key) {
-			$$('.' + key).each(function(el, key) {
-				value(el);
-			});
-		});
-	},
+            removeClassHook: function(name) {
+                delete(this.classhooks[name]);
+            },
 
-	/**
-	 * Local helper for form elements to submit the form.
-	 */
+            /**
+             * apply all class handlers to their elements.
+             */
+            applyClassHooks: function() {
+                Object.each(this.classhooks, function(value, key) {
+                    $$('.' + key).each(function(el, key) {
+                        value(el);
+                    });
+                });
+            },
 
-	applySubmitOnChange: function(el) {
-		$(el).addEvent('change', function() {
-			$(el).getParent('form').submit()
-		});
-	},
+            /**
+             * Local helper for form elements to submit the form.
+             */
 
-	applySubmitOnClick: function(el) {
-		$(el).addEvent('click', function() {
-			$(el).getParent('form').submit()
-		});
-	},
+            applySubmitOnChange: function(el) {
+                $(el).addEvent('change', function() {
+                    $(el).getParent('form').submit()
+                });
+            },
 
-});
+            applySubmitOnClick: function(el) {
+                $(el).addEvent('click', function() {
+                    $(el).getParent('form').submit()
+                });
+            },
 
-var MetaModelsFE = new MetaModels();
+        });
 
-// apply all class hooks on domready
-window.addEvent('domready', function() {
-	MetaModelsFE.applyClassHooks();
-});
+        var MetaModelsFE = new MetaModels();
+
+        // apply all class hooks on domready
+        window.addEvent('domready', function() {
+            MetaModelsFE.applyClassHooks();
+        });
+
+    })(mootools);
+
+}
+// Check if we have jQuery
+else if (typeof jQuery !== 'undefined') {
+
+    (function($) {
+        $.MetaModels = {
+
+            // VARS ------------------------------------------------------------
+            /**
+             * Options
+             */
+            options: {},
+
+            /**
+             * List for additions functions
+             */
+            classhooks: {},
+
+            /**
+             * List for additions events
+             */
+            events: [],
+
+            // CORE ------------------------------------------------------------
+            /**
+             * Init
+             */
+            init: function(opts) {
+                // Set the given params
+                this.options = $.extend(this.options, opts);
+
+                this.addClassHook('submitonchange', this.applySubmitOnChange);
+                this.addClassHook('submitonclick', this.applySubmitOnClick);
+            },
+
+            // DEFAULT ---------------------------------------------------------
+            /**
+             * Local helper for form elements to submit the form.
+             */
+            applySubmitOnChange: function(el, helper) {
+                helper.bindEvent({
+                    object: el,
+                    type: 'change',
+                    func: function(event) {
+                        $(this).parents('form:first')[0].submit()
+                    }
+                });
+            },
+
+            applySubmitOnClick: function(el, helper) {
+                helper.bindEvent({
+                    object: el,
+                    type: 'click',
+                    func: function(event) {
+                        $(this).parents('form:first')[0].submit()
+                    }
+                });
+            },
+
+            // API ------------------------------------------------------------
+            /**
+             * Hook functions, for adding/remove new functions
+             */
+            addClassHook: function(name, callback) {
+                this.classhooks[name] = callback;
+            },
+
+            removeClassHook: function(name) {
+                delete(this.classhooks[name]);
+            },
+
+            /**
+             * apply all class handlers to their elements.
+             */
+            applyClassHooks: function() {
+                var self = this;
+
+                $.each(this.classhooks, function(key, value) {
+                    $('.' + key).each(function(key1, el) {
+                        value(el, self);
+                    });
+                });
+            },
+
+            // EVENTS ----------------------------------------------------------
+            /**
+             * Bind given event
+             * 
+             * @param {object} objEvent
+             */
+            bindEvent: function(objEvent) {
+                this.events.push(objEvent);                
+                $(objEvent.object).bind(objEvent.type, objEvent.func);
+            },
+
+            /**
+             * Unbind given event
+             * 
+             * @param {object} objEvent
+             * @param {boolean} blnNotRemove
+             */
+            unbindEvent: function(objEvent, blnNotRemove) {
+                var intIndex = null;
+
+                $(objEvent.object).unbind(objEvent.type);
+
+                if (blnNotRemove !== true) {
+                    for (var i = 0; i < this.events.length; i++) {
+                        if (objEvent.object === this.events[i].object && objEvent.type === this.events[i].type) {
+                            intIndex = i;
+                            break;
+                        }
+                    }
+                    if (intIndex !== null) {
+                        this.events.splice(intIndex, 1);
+                    }
+                }
+            },
+
+            /**
+             * Unbind events for this class
+             */
+            unbindEvents: function() {
+                var arrStore = [];
+
+                for (var i = 0; i < this.events.length; i++) {
+                    if (this.onLoadWindowScroll === this.events[i]) {
+                        arrStore.push(this.onLoadWindowScroll);
+                        continue;
+                    }
+
+                    this.unbindEvent(this.events[i], true);
+                }
+
+                this.events = arrStore;
+            },
+        }
+
+        $(document).ready(function() {
+            $.MetaModels.init();
+            $.MetaModels.applyClassHooks();
+        });
+
+    })(jQuery);
+}
