399f03a1caf0f4e704377058c3f42db619d9f7d0
Ondrej <Sam256@web.de>
2013-05-29 15:20:42 +0200

    Fixed missing MetaModelBreadcrumbBuilder class

--- src/system/modules/metamodels/config/autoload.php
+++ src/system/modules/metamodels/config/autoload.php
@@ -84,6 +84,7 @@ ClassLoader::addClasses(array
 	'MetaModelTemplate'                     => 'system/modules/metamodels/MetaModelTemplate.php',
 	'MetaModelsBackendSupport'              => 'system/modules/metamodels/MetaModelsBackendSupport.php',
 	'MetaModelsUpgradeHandler'              => 'system/modules/metamodels/MetaModelsUpgradeHandler.php',
+	'MetaModelBreadcrumbBuilder'            => 'system/modules/metamodels/MetaModelBreadcrumbBuilder.php',
 	'ModuleMetaModelFrontendFilter'         => 'system/modules/metamodels/ModuleMetaModelFrontendFilter.php',
 	'ModuleMetaModelList'                   => 'system/modules/metamodels/ModuleMetaModelList.php',
 	'TableContent'                          => 'system/modules/metamodels/TableContent.php',
