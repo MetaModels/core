44bc6fd27efaa4d111b9bfbd36c938bb536d8274
Mini Model <minimodel@metamodel.me>
2013-06-19 08:12:27 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/it/default.php
+++ src/system/modules/metamodels/languages/it/default.php
@@ -8,8 +8,28 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-13T16:12:25+02:00
+ * last-updated: 2013-06-13T16:13:08+02:00
  */
 
-
-$GLOBALS['TL_LANG']['CTE']['metamodels'] = 'Elementi MetaModel';
+$GLOBALS['TL_LANG']['BRD']['metamodel_attribute']                 = 'Attributi di "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodels']                          = 'MetaModels';
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['0']              = 'Lista MetaModel';
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['1']              = 'Aggiungi una lista di elementi MetaModel in un articolo';
+$GLOBALS['TL_LANG']['CTE']['metamodels']                          = 'Elementi MetaModel';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendclearall']['0']    = 'MetaModel cancella tutti';
+$GLOBALS['TL_LANG']['ERR']['invalidTableName']                    = 'Il nome della tabella non è valido';
+$GLOBALS['TL_LANG']['MSC']['details']                             = 'Dettagli';
+$GLOBALS['TL_LANG']['MSC']['metamodels_help']                     = 'Abbiamo bisogno del vostro aiuto!';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['filter']['0']          = 'Filtro';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['limit']['0']           = 'Limite';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['search']['0']          = 'Ricerca';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['sort']['0']            = 'Ordinamento';
+$GLOBALS['TL_LANG']['MSC']['sorting']                             = 'Ordinamento';
+$GLOBALS['TL_LANG']['MSC']['template_in_theme']                   = '%s (%s)';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['clr']['0']                = 'clr';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['m12']['0']                = 'm12';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['w50']['0']                = 'w50';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_add']    = '+';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_remove'] = '-';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['select_all']    = 'Seleziona tutto';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['submit']        = 'Filtro';
