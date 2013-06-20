ee5c4c176a68fcb1c3116cc37685b3132f8be21e
Mini Model <minimodel@metamodel.me>
2013-06-20 12:12:49 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/it/modules.php
+++ src/system/modules/metamodels/languages/it/modules.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-19T08:12:25+02:00
+ * last-updated: 2013-06-19T12:14:01+02:00
  */
 
 $GLOBALS['TL_LANG']['FMD']['metamodel_list']['0']              = 'Lista MetaModel';
@@ -16,4 +16,5 @@ $GLOBALS['TL_LANG']['FMD']['metamodels']['0']                  = 'MetaModels';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendclearall']['0'] = 'MetaModel cancella tutti';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendclearall']['1'] = 'Aggiungi dei clear all per tutti i filtri del frontend.';
 $GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['0']   = 'Filtri di frontend  di MetaModel';
+$GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['1']   = 'Aggiungi un filtro di frontend per il MetaModel';
 $GLOBALS['TL_LANG']['MOD']['metamodels']['0']                  = 'MetaModels';
---
+++ src/system/modules/metamodels/languages/it/tl_content.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/it/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-30T04:10:43+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_content']['metamodel']['0']                  = 'MetaModel';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_filtering']['0']        = 'Seleziona le impostazioni dei filtri da aplicare';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_limit']['0']            = 'Numero massimo di elementi';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_offset']['0']           = 'Lista offset';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_sortby']['0']           = 'Ordinamento';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_sortby']['1']           = 'Selezionare il tipo di ordinamento';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_sortby_direction']['1'] = 'Ordine ascendente o discendente';
+$GLOBALS['TL_LANG']['tl_content']['metamodel_use_limit']['0']        = 'Utilizza offset e limiti nella lista';
+$GLOBALS['TL_LANG']['tl_content']['mm_filter_legend']                = 'Filtro MetaModels';
