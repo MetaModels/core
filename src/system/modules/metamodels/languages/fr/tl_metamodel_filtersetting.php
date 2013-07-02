684f7b56453344f1ceaee198aad303432cae9c91
Mini Model <minimodel@metamodel.me>
2013-07-02 16:13:02 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/fr/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodels/languages/fr/tl_metamodel_filtersetting.php
@@ -8,12 +8,13 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T23:07:50+02:00
+ * last-updated: 2013-05-18T20:12:21+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id']['0']  = 'Attribut';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['config_legend'] = 'Configuration';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['edit']['0']     = 'Éditer le paramètre';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['label']['0']    = 'Libellé';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['0']      = 'Nouveau';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['1']      = 'Créer un nouveau paramètre.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['template']['0'] = 'Modèle';
