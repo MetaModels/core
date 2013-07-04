2b8a4fa7c4c8da538f6114ac23e5c823018472d2
Mini Model <minimodel@metamodel.me>
2013-07-04 20:12:46 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/it/explain.php
+++ src/system/modules/metamodels/languages/it/explain.php
@@ -8,11 +8,28 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-23T04:12:22+02:00
+ * last-updated: 2013-07-04T16:13:02+02:00
  */
 
 $GLOBALS['TL_LANG']['XPL']['customsql']['0']['0'] = 'Anteprima';
 $GLOBALS['TL_LANG']['XPL']['customsql']['0']['1'] = 'Digitare una query SQL che deve essere eseguita.<br />
 		E \'necessario che questa query restituisce almeno una colonna denominata "id".
 		';
+$GLOBALS['TL_LANG']['XPL']['customsql']['1']['0'] = 'Esempio 1<br />query semplice';
+$GLOBALS['TL_LANG']['XPL']['customsql']['1']['1'] = '<pre>SELECT id FROM mm_mymetamodel WHERE page_id=1</pre>
+⇥⇥Questo seleziona tutti gli ID dalla tabella <em> mm_mymetamodel </ em> che hanno il valore <em> page_id = 1 </ em> ⏎
+⇥ ⇥
+		';
+$GLOBALS['TL_LANG']['XPL']['customsql']['2']['0'] = 'Esempio 2<br />Riferimento nome tabella';
+$GLOBALS['TL_LANG']['XPL']['customsql']['2']['1'] = '<pre>SELECT id FROM {{table}} WHERE page_id=1</pre>⏎
+⇥⇥Questa è una query identica a quella dell\'esempio 1, ma il nome della tabella è quello del MetaModel corrente (<em>mm_mymetamodel </ em> come da esempio precedente) che verrà inserito nella query.⏎
+⇥⇥
+		';
 $GLOBALS['TL_LANG']['XPL']['customsql']['3']['0'] = 'Insert tags';
+$GLOBALS['TL_LANG']['XPL']['customsql']['4']['0'] = 'Secure insert tags';
+$GLOBALS['TL_LANG']['XPL']['customsql']['4']['1'] = 'I Secure insert tags sono simili ai classici insert tags, ma i loro valori mantenuti all\'interno della query.<br />⏎
+⇥⇥Pertanto si potrebbe essere meglio utilizzare l\'equivalente sicuro se non si sa esattamente cosa si sta facendo.<br />⏎
+⇥⇥La modalità di utilizzo è:⏎
+⇥⇥<pre>{{secure::page::id}}</pre>';
+$GLOBALS['TL_LANG']['XPL']['customsql']['5']['0'] = 'Sorgente dei parametri';
+$GLOBALS['TL_LANG']['XPL']['customsql']['6']['0'] = 'Esempio 3<br />Utilizzo di filtri di sorgenti parametri complessi ';
