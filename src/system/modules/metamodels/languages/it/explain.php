f5f2d90b003f07d9fb55f12d5e0f1ae2cdd8243e
Mini Model <minimodel@metamodel.me>
2013-07-04 16:13:03 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/it/explain.php
+++ src/system/modules/metamodels/languages/it/explain.php
@@ -8,8 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T06:40:07+02:00
+ * last-updated: 2013-06-23T04:12:22+02:00
  */
 
-
+$GLOBALS['TL_LANG']['XPL']['customsql']['0']['0'] = 'Anteprima';
+$GLOBALS['TL_LANG']['XPL']['customsql']['0']['1'] = 'Digitare una query SQL che deve essere eseguita.<br />
+		E \'necessario che questa query restituisce almeno una colonna denominata "id".
+		';
 $GLOBALS['TL_LANG']['XPL']['customsql']['3']['0'] = 'Insert tags';
