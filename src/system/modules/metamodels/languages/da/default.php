677aee90cc4ac337f8cd1e16831c900be71fa4b1
Mini Model <minimodel@metamodel.me>
2013-07-06 00:12:54 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels/languages/da/default.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/da/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-06-11T01:40:38+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['BRD']['metamodel_attribute']                                     = 'Egenskaber for "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_dca']                                           = 'Alle indtastningsvisninger for "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting']                                    = 'Indtastningsvisninger i "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting_subpalette']                         = 'Underområder for "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_filter']                                        = 'Alle filtre for "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_filtersetting']                                 = 'Filter indstillinger i "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_rendersetting']                                 = 'Gengivelsesindstillinger i "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_rendersettings']                                = 'Alle gengivelsesindstillinger for "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodels']                                              = 'MetaModels';
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['0']                                  = 'MetaModel liste';
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['1']                                  = 'Tilføjer en liste af MetaModel indhold til artiklen.';
+$GLOBALS['TL_LANG']['CTE']['metamodels']                                              = 'MetaModel elementer';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendclearall']['0']                        = 'MetaModel ryd alt';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendclearall']['1']                        = 'Tilføjer en ryd alt knap for alle frontend filtre.';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['0']                          = 'MetaModel frontend filter';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['1']                          = 'Tilføjer et frontend filter for en MetaModel.';
+$GLOBALS['TL_LANG']['ERR']['activate_extension']                                      = 'Aktiver venligst den påkrævede udvidelse "%s" (%s)';
+$GLOBALS['TL_LANG']['ERR']['columnExists']                                            = 'Der eksisterer allerede en egenskab med dette kolonne navn.';
+$GLOBALS['TL_LANG']['ERR']['install_extension']                                       = 'Installer venligst den påkrævede udvidelse "%s" (%s)';
+$GLOBALS['TL_LANG']['ERR']['invalidTableName']                                        = 'Tabel navnet er ugyldigt.';
+$GLOBALS['TL_LANG']['ERR']['no_palette']                                              = 'Forsøgt at tilgå MetaModel "%s" uden indtastningsvisninger for nuværende bruger %s.';
+$GLOBALS['TL_LANG']['ERR']['no_view']                                                 = 'Fosøg på at tilgå MetaModel "%s" uden visning for bruger %s.';
+$GLOBALS['TL_LANG']['MSC']['details']                                                 = 'Oplysninger';
+$GLOBALS['TL_LANG']['MSC']['metamodel_edit_as_child']['label']                        = 'Rediger "%s" for %%s';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['editRecord']                   = 'Rediger filter indstilling %%s for filter "%s" i MetaModel "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['label']                        = 'Filter "%s" i MetaModel "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersettings_parameter']['simplelookup']['0'] = 'Filtreringsværdi for egenskaben "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['editRecord']                   = 'Rediger egenskabsindstilling %%s for gengivelsesindstilling "%s" i MetaModel "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['label']                        = 'Gengivelsesindstilling "%s" i MetaModel "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodels_contributor']                                  = 'Tak til alle brugere for tickets, foreslag og oversættelser';
+$GLOBALS['TL_LANG']['MSC']['metamodels_help']                                         = 'Vi har brug for din hjælp!';
+$GLOBALS['TL_LANG']['MSC']['noItemsMsg']                                              = 'Intet matchede din søgning.';
+$GLOBALS['TL_LANG']['MSC']['no_theme']                                                = 'globalt omfang';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['filter']['0']                              = 'Filtrer';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['limit']['0']                               = 'Begræns';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['search']['0']                              = 'Søg';
+$GLOBALS['TL_LANG']['MSC']['panelLayout']['sort']['0']                                = 'Sorter';
+$GLOBALS['TL_LANG']['MSC']['sorting']                                                 = 'Sortering';
+$GLOBALS['TL_LANG']['MSC']['template_in_theme']                                       = '%s (%s)';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['clr']['0']                                    = 'clr';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['clr']['1']                                    = 'Ryd alle floats.';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['long']['0']                                   = 'long';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['long']['1']                                   = 'Giver input feltet et span på to kolonner.';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['m12']['0']                                    = 'm12';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['m12']['1']                                    = 'Tilføj en 12 pixel top margin til elementet (benyttes ved enkeltstående checkboxes).';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['w50']['0']                                    = 'w50';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['w50']['1']                                    = 'Sæt feltets bredte til 50% og venstrestil det (float:left).';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['w50x']['0']                                   = 'w50x';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['wizard']['0']                                 = 'guide';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_add']                        = '+';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_remove']                     = '-';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['clear_all']                         = 'Ryd alle filtre';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['do_not_filter']                     = 'Ingen filtrering';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['no_combinations']                   = '(Ingen matchende kombinationer blev fundet.)';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['select_all']                        = 'Vælg alle';
+$GLOBALS['TL_LANG']['metamodels_frontendfilter']['submit']                            = 'Filtrer';
