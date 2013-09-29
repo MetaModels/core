843fa01d4c9e48633d419b5496136829598b4400
Mini Model <minimodel@metamodel.me>
2013-09-29 04:13:12 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels/languages/fr/tl_metamodel_dca_combine.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T06:40:07+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['be_group']['0']     = 'Groupe back office';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['be_group']['1']     = 'Le groupe d\'utilisateur back office auquel la combinaison s\'applique.';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_combiner']['0'] = 'Permissions pour les masques de saisie et vues';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_combiner']['1'] = 'Pour les groupes d\'utilisateur front office selectionnés (le cas échéant) et les groupes d\'utilisateur back office selectionnés (le cas échant) utilisez la palette et la vue sélectionnées.';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_id']['0']       = 'Le masque de saisie';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_id']['1']       = 'Le masque de saisie auquel la combinaison s\'applique.';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['fe_group']['0']     = 'Groupe front office';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['fe_group']['1']     = 'Le groupe d\'utilisateur front office auquel la combinaison s\'applique.';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['sysadmin']          = 'Administrateur';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['view_id']['0']      = 'Le paramètre de rendu';
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['view_id']['1']      = 'La vue à laquelle la combinaison s\'applique';
--- src/system/modules/metamodels/languages/fr/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodels/languages/fr/tl_metamodel_filtersetting.php
@@ -8,32 +8,60 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-07-10T10:02:32+02:00
+ * last-updated: 2013-09-29T00:13:17+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['all_langs']['0']        = 'Cherche dans toutes les langues';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['all_langs']['1']        = 'Cocher si vous voulez effectuer une recherche indépendante du langage. Si non coché, seuls les langages courant activés seront recherché.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['allow_empty']['0']      = 'Permettre valeur vide';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['allow_empty']['1']      = 'Coché si vous souhaitez permettre cette valeur du filtre d\'être vide. si coché et que le paramètre contient une valeur vide, ce filtre ce comportera comme si il n\'était pas définit.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id']['0']          = 'Attribut';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id']['1']          = 'Attribut auquel ce paramètre se réfère.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['comment']['0']          = 'Commentaire';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['comment']['1']          = 'Un court commentaire pour décrire l\'objet de ce paramètre de filtre.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['config_legend']         = 'Configuration';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['edit']['0']             = 'Éditer le paramètre';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['enabled']['0']          = 'Permis';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['enabled']['1']          = 'Permettre ce paramètre de filtre.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fid']['0']              = 'collection parente';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fid']['1']              = 'La collection des paramètre de filtre à laquelle ce paramètre appartiens.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['label']['0']            = 'Libellé';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['0']              = 'Nouveau';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['1']              = 'Créer un nouveau paramètre.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['predef_param']['0']     = 'Paramètre statique';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['stop_after_match']['0'] = 'Arrêter après la première correspondance';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['stop_after_match']['1'] = 'Cocher si vous souhaitez que ce paramètre de filtre arrête l\'exécution de ses règles lèguées après que le premier sous ensemble ait trouvé des correspondances.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['template']['0']         = 'Modèle';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['title_legend']          = 'Type';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['type']['0']             = 'Type';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['type']['1']             = 'Le type de ce paramètre.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['urlparam']['0']         = 'Paramètre URL';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['urlparam']['1']         = 'Le paramètre d\'URL qui doit être appliqué à l\'attribut sélectionné. Le paramètre spécial <em>"auto_item"</em> peut aussi être utilisé, spécialement pour les alias de colonnes.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['all_langs']['0']            = 'Cherche dans toutes les langues';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['all_langs']['1']            = 'Cocher si vous voulez effectuer une recherche indépendante du langage. Si non coché, seuls les langages courant activés seront recherché.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['allow_empty']['0']          = 'Permettre valeur vide';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['allow_empty']['1']          = 'Coché si vous souhaitez permettre cette valeur du filtre d\'être vide. si coché et que le paramètre contient une valeur vide, ce filtre ce comportera comme si il n\'était pas définit.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id']['0']              = 'Attribut';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id']['1']              = 'Attribut auquel ce paramètre se réfère.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['blankoption']['0']          = 'Option vide';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['blankoption']['1']          = 'Montrer les options vide dans la sélection.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['comment']['0']              = 'Commentaire';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['comment']['1']              = 'Un court commentaire pour décrire l\'objet de ce paramètre de filtre.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['config_legend']             = 'Configuration';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['copy']['0']                 = 'Copier définition paramètre filtre';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['copy']['1']                 = 'Copier paramètre filtre ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['customsql']['0']            = 'Requête SQL personalisée';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['customsql']['1']            = 'La requête SQL qui doit être éxecutée, les balises sont supportées.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delete']['0']               = 'Supprimer paramètre filtre';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['delete']['1']               = 'Supprimer paramètre filtre ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['edit']['0']                 = 'Éditer le paramètre';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['edit']['1']                 = 'Editer paramètre filtre ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['enabled']['0']              = 'Permis';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['enabled']['1']              = 'Permettre ce paramètre de filtre.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fid']['0']                  = 'collection parente';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fid']['1']                  = 'La collection des paramètre de filtre à laquelle ce paramètre appartiens.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['label']['0']                = 'Etiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['label']['1']                = 'Montrer l\'étiquette au lieu du nom d\'attribut';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['0']                  = 'Nouveau';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['new']['1']                  = 'Créer un nouveau paramètre.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0']         = 'Balises restante seulement';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1']         = 'Montrer seulement les options qui sont assignées quelque part après que le premier filtre soit défini.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']             = 'Assigner seulement des balises';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']             = 'Montrer seulement les options assignés quelquepart dans le MetaModel.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['predef_param']['0']         = 'Paramètre statique';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['predef_param']['1']         = 'Cocher si vous souhaitez être capable de définir la valeur de ce paramètre dans les listes parent (modules, éléments, etc.)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['show']['0']                 = 'Détails paramètre filtre';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['show']['1']                 = 'Montere détail du paramètre de filtre ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['stop_after_match']['0']     = 'Arrêter après la première correspondance';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['stop_after_match']['1']     = 'Cocher si vous souhaitez que ce paramètre de filtre arrête l\'exécution de ses règles lèguées après que le premier sous ensemble ait trouvé des correspondances.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['template']['0']             = 'Modèle';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['title_legend']              = 'Type';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['type']['0']                 = 'Type';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['type']['1']                 = 'Le type de ce paramètre.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['_comment_']     = '<span title="%s"><sup>(?)</sup></span>';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['_default_']     = '%s <strong>%s</strong> %s <em>[%s]</em>';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['conditionand']  = '%s <strong>%s</strong> %s <br /> articles qui sont mentionnés dans tout les résultats.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['conditionor']   = '%s <strong>%s</strong> %s <br /> articles qui sont mentionnés dans aucun résultat.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['fefilter']      = '%s <strong>%s</strong> %s <br /> pour l\'attribut <em>%s</em> (URL parameter: %s)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typedesc']['simplelookup']  = '%s <strong>%s</strong> %s <br /> on attribute <em>%s</em> (URL parameter: %s)';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['conditionand'] = 'condition AND';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['conditionor']  = 'condition OR';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['customsql']    = 'SQL personalisé';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['idlist']       = 'Ensemble prédéfini';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['simplelookup'] = 'Recherche simple';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['urlparam']['0']             = 'Paramètre URL';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['urlparam']['1']             = 'Le paramètre d\'URL qui doit être appliqué à l\'attribut sélectionné. Le paramètre spécial <em>"auto_item"</em> peut aussi être utilisé, spécialement pour les alias de colonnes.';
