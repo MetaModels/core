2d7256ef9aa05ca12da1d8283d503cec6070b4ac
Mini Model <minimodel@metamodel.me>
2013-10-15 00:13:51 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels/languages/sk/modules.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/sk/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-15T00:13:34+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['metamodel_list']['0'] = 'Zoznam Metamodel-ov';
