1721c5c6037bd40eaa6d50cd58a7d75d17711354
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-12-24 03:10:36 +0100

    Remove autoload hack.

--- src/system/modules/metamodels/config/config.php
+++ src/system/modules/metamodels/config/config.php
@@ -14,56 +14,6 @@
  * @filesource
  */
 
-
-if (!is_array($GLOBALS['MM_AUTOLOAD']))
-{
-	$GLOBALS['MM_AUTOLOAD'] = array();
-}
-
-$GLOBALS['MM_AUTOLOAD'][] = dirname(__DIR__);
-$GLOBALS['MM_AUTOLOAD'][] = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'deprecated';
-
-// For the moment, we add our autoloader at the end for non composerized Contao 2.X compat.
-if (version_compare(VERSION, '3.0', '<')/* && !class_exists('Composer\Autoload\ClassLoader')*/)
-{
-	function metamodels_autoload($className)
-	{
-		$className = ltrim($className, '\\');
-		$fileName  = '';
-
-		if ($lastNsPos = strripos($className, '\\'))
-		{
-			$namespace = substr($className, 0, $lastNsPos);
-			$className = substr($className, $lastNsPos + 1);
-			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-		}
-		$fileName .= $className . '.php';
-
-		foreach ($GLOBALS['MM_AUTOLOAD'] as $baseDir)
-		{
-			if (file_exists($baseDir . DIRECTORY_SEPARATOR . $fileName))
-			{
-				require $baseDir . DIRECTORY_SEPARATOR . $fileName;
-
-				// Tell the Contao 2.X auto loader cache that the class is available.
-				if (class_exists('Cache'))
-				{
-					\Cache::getInstance()->{'classFileExists-' . $className} = true;
-				}
-				\FileCache::getInstance('classes')->$className = true;
-
-				return true;
-			}
-		}
-
-		return null;
-	}
-
-	spl_autoload_unregister('__autoload');
-	spl_autoload_register('metamodels_autoload', true, false);
-	spl_autoload_register('__autoload');
-}
-
 // Preserve values by extensions but insert as first entry after 'system'.
 $arrOld = (array)$GLOBALS['BE_MOD']['metamodels'];
 unset($GLOBALS['BE_MOD']['metamodels']);
