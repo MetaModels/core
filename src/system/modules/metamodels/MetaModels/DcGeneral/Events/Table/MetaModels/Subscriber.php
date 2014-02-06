0f5d37a5acc7669846117f0d52e4638a58d1221b
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-02-06 04:00:37 +0100

    code style.

--- src/system/modules/metamodels/MetaModels/DcGeneral/Events/Table/MetaModels/Subscriber.php
+++ src/system/modules/metamodels/MetaModels/DcGeneral/Events/Table/MetaModels/Subscriber.php
@@ -16,19 +16,28 @@
 
 namespace MetaModels\DcGeneral\Events\Table\MetaModels;
 
-use DcGeneral\Contao\BackendBindings;
+use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
+use ContaoCommunityAlliance\Contao\Bindings\Events\Image\GenerateHtmlEvent;
+use ContaoCommunityAlliance\Contao\Bindings\Events\Image\ResizeImageEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\DecodePropertyValueForWidgetEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\EncodePropertyValueFromWidgetEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\ModelToLabelEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\GetOperationButtonEvent;
 use DcGeneral\Contao\View\Contao2BackendView\Event\GetGlobalButtonEvent;
 
+/**
+ * Handles event operations on tl_metamodel.
+ *
+ * @package MetaModels\DcGeneral\Events\Table\MetaModels
+ */
 class Subscriber
 {
 	/**
 	 * Clear the button if the User is not admin.
 	 *
-	 * @param GetOperationButtonEvent $event
+	 * @param GetOperationButtonEvent $event The event.
+	 *
+	 * @return void
 	 */
 	public static function getOperationButton(GetOperationButtonEvent $event)
 	{
@@ -41,7 +50,9 @@ class Subscriber
 	/**
 	 * Clear the button if the User is not admin.
 	 *
-	 * @param GetGlobalButtonEvent $event
+	 * @param GetGlobalButtonEvent $event The event.
+	 *
+	 * @return void
 	 */
 	public static function getGlobalButton(GetGlobalButtonEvent $event)
 	{
@@ -50,31 +61,45 @@ class Subscriber
 			$event->setHtml('');
 		}
 	}
+
+	/**
+	 * Render a model in the backend list.
+	 *
+	 * @param ModelToLabelEvent $event The event.
+	 *
+	 * @return void
+	 */
 	public static function modelToLabel(ModelToLabelEvent $event)
 	{
 		$model = $event->getModel();
 
 		$translator = $event->getEnvironment()->getTranslator();
 
-		if(!($model && \Database::getInstance()->tableExists($model->getProviderName(), null, true)))
+		if (!($model && \Database::getInstance()->tableExists($model->getProviderName(), null, true)))
+		{
 			return;
+		}
 
 		$strLabel = vsprintf($event->getLabel(), $event->getArgs());
 
-		// add image
 		$strImage = '';
 		if ($model->getProperty('addImage'))
 		{
-			$arrSize = deserialize($model->getProperty('size'));
+			$arrSize    = deserialize($model->getProperty('size'));
+			$imageEvent = new ResizeImageEvent($model->getProperty('singleSRC'), $arrSize[0], $arrSize[1], $arrSize[2]);
+
+			$event->getEnvironment()->getEventPropagator()->propagate(ContaoEvents::IMAGE_RESIZE, $event);
+
 			$strImage = sprintf('<div class="image" style="padding-top:3px"><img src="%s" alt="%%1$s" /></div> ',
-				BackendBindings::getImage($model->getProperty('singleSRC'), $arrSize[0], $arrSize[1], $arrSize[2]),
+				$imageEvent->getImage(),
 				htmlspecialchars($strLabel)
 			);
 		}
 
-		// count items
-		$objCount = \Database::getInstance()->prepare("SELECT count(*) AS itemCount FROM ".$model->getProperty('tableName'))
+		$objCount = \Database::getInstance()
+			->prepare('SELECT count(*) AS itemCount FROM ' . $model->getProperty('tableName'))
 			->execute();
+
 		$itemCount =  sprintf(
 			' <span style="color:#b3b3b3; padding-left:3px">[%s]</span>',
 			$translator->translatePluralized('itemFormat', $objCount->itemCount, 'tl_metamodel', array($objCount->itemCount))
@@ -83,7 +108,13 @@ class Subscriber
 		$event->setArgs('<span class="name">' . $strLabel . $itemCount . '</span>' . $strImage);
 	}
 
-
+	/**
+	 * Decode a language array.
+	 *
+	 * @param DecodePropertyValueForWidgetEvent $event The event.
+	 *
+	 * @return void
+	 */
 	public static function fixLangArray(DecodePropertyValueForWidgetEvent $event)
 	{
 		$langValues = (array)$event->getValue();
@@ -99,6 +130,13 @@ class Subscriber
 		$event->setValue($output);
 	}
 
+	/**
+	 * Encode a language array.
+	 *
+	 * @param EncodePropertyValueFromWidgetEvent $event The event.
+	 *
+	 * @return void
+	 */
 	public static function unfixLangArray(EncodePropertyValueFromWidgetEvent $event)
 	{
 		$langValues  = (array)$event->getValue();
@@ -109,20 +147,22 @@ class Subscriber
 			$langCode = $subValue['langcode'];
 			unset($subValue['langcode']);
 
-			// we clear all subsequent fallbacks after we have found one.
-			if($hasFallback)
+			// We clear all subsequent fallbacks after we have found one.
+			if ($hasFallback)
 			{
 				$varSubValue['isfallback'] = '';
 			}
-			if($subValue['isfallback'])
+
+			if ($subValue['isfallback'])
 			{
 				$hasFallback = true;
 			}
+
 			$output[$langCode] = $subValue;
 		}
 
-		// if no fallback has been set, use the first language available.
-		if((!$hasFallback) && count($output))
+		// If no fallback has been set, use the first language available.
+		if ((!$hasFallback) && count($output))
 		{
 			$output[$langValues[0]['langcode']]['isfallback'] = '1';
 		}
