55f4b9e26bf9949e69b82724d235852eacedcae1
Andreas Isaak <andy.jared@googlemail.com>
2014-03-09 22:58:56 +0100

    Fix #291

--- src/system/modules/metamodels/MetaModelFrontendFilter.php
+++ src/system/modules/metamodels/MetaModelFrontendFilter.php
@@ -254,7 +254,7 @@ class MetaModelFrontendFilter extends Frontend
 	 */
 	public function generateClearAll($strContent, $strTemplate)
 	{
-		if (substr($strTemplate, 0, 7) === 'fe_page')
+		if(strpos($strTemplate,'fe_page') === 0)
 		{
 			if (preg_match_all(
 				'#\[\[\[metamodelfrontendfilterclearall::(ce|mod)::([^\]]*)\]\]\]#',
