1526170d73097b6f86cf0b78120e12e14e3990f4
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-04-10 06:46:05 +0200

    Overhaul the language file. Add missing keys.

--- src/system/modules/metamodels/languages/en/tl_metamodel_item.php
+++ src/system/modules/metamodels/languages/en/tl_metamodel_item.php
@@ -17,20 +17,29 @@
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_metamodel_item']['new']           = array('New item', 'Create new item');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['edit']          = array('Edit item', 'Edit item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['copy']          = array('Copy item', 'Copy item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['createvariant'] = array('New variant', 'Create a new variant of item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['cut']           = array('Move item', 'Move item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['delete']        = array('Delete item', 'Delete item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['show']          = array('Item details', 'Show details of item ID %s');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['editheader']    = array('Edit item type', 'Edit the item type');
-$GLOBALS['TL_LANG']['tl_metamodel_item']['fields']        = array('Manage attributes', 'Manage attributes of this MetaModel');
+$GLOBALS['TL_LANG']['tl_metamodel_item']['new'][0]           = 'New item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['new'][1]           = 'Create new item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['edit'][0]          = 'Edit item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['edit'][1]          = 'Edit item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['copy'][0]          = 'Copy item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['copy'][1]          = 'Copy item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['createvariant'][0] = 'New variant';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['createvariant'][1] = 'Create a new variant of item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['cut'][0]           = 'Move item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['cut'][1]           = 'Move item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['delete'][0]        = 'Delete item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['delete'][1]        = 'Delete item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['show'][0]          = 'Item details';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['show'][1]          = 'Show details of item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['editheader'][0]    = 'Edit item type';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['editheader'][1]    = 'Edit the item type';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['pasteafter'][0]    = 'Create new item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['pasteafter'][1]    = 'Create a new item after item ID %s';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['pasteinto'][0]     = 'Create new item';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['pasteinto'][1]     = 'Create a new item in item ID %s';
 
-$GLOBALS['TL_LANG']['tl_metamodel_item']['pastenew']      = array('Create new item', 'Create a new item after item ID %s');
-
-
-$GLOBALS['TL_LANG']['tl_metamodel_item']['varbase']       = array('Is variant base', 'Check this if you want to make this the base for the current variant group');
+$GLOBALS['TL_LANG']['tl_metamodel_item']['varbase'][0] = 'Is variant base';
+$GLOBALS['TL_LANG']['tl_metamodel_item']['varbase'][1] = 'Check this if you want to make this the base for the current variant group';
 
 $GLOBALS['TL_LANG']['tl_metamodel_item']['editRecord'] = 'Edit item %s';
 $GLOBALS['TL_LANG']['tl_metamodel_item']['newRecord']  = 'Create a new item';
