73cd712dbf6ecdabc01494a8da0f5695a357dcec
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-04-10 05:42:54 +0200

    Add checks for reserved MySQL words.

--- src/system/modules/metamodels/MetaModels/Helper/TableManipulation.php
+++ src/system/modules/metamodels/MetaModels/Helper/TableManipulation.php
@@ -74,6 +74,43 @@ class TableManipulation
 	const STATEMENT_DROP_COLUMN = 'ALTER TABLE %s DROP COLUMN %s';
 
 	/**
+	 * List of reserved MySQL identifiers.
+	 *
+	 * @var string[]
+	 */
+	protected static $reservedWords = array(
+		// MySQL 5.5 and previous.
+		'ACCESSIBLE', 'ADD', 'ALL', 'ALTER', 'ANALYZE', 'AND', 'AS', 'ASC', 'ASENSITIVE', 'BEFORE', 'BETWEEN', 'BIGINT',
+		'BINARY', 'BLOB', 'BOTH', 'BY', 'CALL', 'CASCADE', 'CASE', 'CHANGE', 'CHAR', 'CHARACTER', 'CHECK', 'COLLATE',
+		'COLUMN', 'CONDITION', 'CONSTRAINT', 'CONTINUE', 'CONVERT', 'CREATE', 'CROSS', 'CURRENT_DATE', 'CURRENT_TIME',
+		'CURRENT_TIMESTAMP', 'CURRENT_USER', 'CURSOR', 'DATABASE', 'DATABASES', 'DAY_HOUR', 'DAY_MICROSECOND',
+		'DAY_MINUTE', 'DAY_SECOND', 'DEC', 'DECIMAL', 'DECLARE', 'DEFAULT', 'DELAYED', 'DELETE', 'DESC', 'DESCRIBE',
+		'DETERMINISTIC', 'DISTINCT', 'DISTINCTROW', 'DIV', 'DOUBLE', 'DROP', 'DUAL', 'EACH', 'ELSE', 'ELSEIF', 'ENCLOSED',
+		'ESCAPED', 'EXISTS', 'EXIT', 'EXPLAIN', 'FALSE', 'FETCH', 'FLOAT', 'FLOAT4', 'FLOAT8', 'FOR', 'FORCE', 'FOREIGN',
+		'FROM', 'FULLTEXT', 'GENERAL', 'GRANT', 'GROUP', 'HAVING', 'HIGH_PRIORITY', 'HOUR_MICROSECOND', 'HOUR_MINUTE',
+		'HOUR_SECOND', 'IF', 'IGNORE', 'IGNORE_SERVER_IDS', 'IN', 'INDEX', 'INFILE', 'INNER', 'INOUT', 'INSENSITIVE',
+		'INSERT', 'INT', 'INT1', 'INT2', 'INT3', 'INT4', 'INT8', 'INTEGER', 'INTERVAL', 'INTO', 'IS', 'ITERATE', 'JOIN',
+		'KEY', 'KEYS', 'KILL', 'LEADING', 'LEAVE', 'LEFT', 'LIKE', 'LIMIT', 'LINEAR', 'LINES', 'LOAD', 'LOCALTIME',
+		'LOCALTIMESTAMP', 'LOCK', 'LONG', 'LONGBLOB', 'LONGTEXT', 'LOOP', 'LOW_PRIORITY', 'MASTER_HEARTBEAT_PERIOD',
+		'MASTER_SSL_VERIFY_SERVER_CERT', 'MATCH', 'MAXVALUE', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT',
+		'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD', 'MODIFIES', 'NATURAL', 'NOT', 'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC',
+		'ON', 'OPTIMIZE', 'OPTION', 'OPTIONALLY', 'OR', 'ORDER', 'OUT', 'OUTER', 'OUTFILE', 'PRECISION', 'PRIMARY',
+		'PROCEDURE', 'PURGE', 'RANGE', 'READ', 'READS', 'READ_WRITE', 'REAL', 'REFERENCES', 'REGEXP', 'RELEASE', 'RENAME',
+		'REPEAT', 'REPLACE', 'REQUIRE', 'RESIGNAL', 'RESTRICT', 'RETURN', 'REVOKE', 'RIGHT', 'RLIKE', 'SCHEMA', 'SCHEMAS',
+		'SECOND_MICROSECOND', 'SELECT', 'SENSITIVE', 'SEPARATOR', 'SET', 'SHOW', 'SIGNAL', 'SLOW', 'SMALLINT', 'SPATIAL',
+		'SPECIFIC', 'SQL', 'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING', 'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS',
+		'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STRAIGHT_JOIN', 'TABLE', 'TERMINATED', 'THEN', 'TINYBLOB', 'TINYINT',
+		'TINYTEXT', 'TO', 'TRAILING', 'TRIGGER', 'TRUE', 'UNDO', 'UNION', 'UNIQUE', 'UNLOCK', 'UNSIGNED', 'UPDATE', 'USAGE',
+		'USE', 'USING', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 'VALUES', 'VARBINARY', 'VARCHAR', 'VARCHARACTER', 'VARYING',
+		'WHEN', 'WHERE', 'WHILE', 'WITH', 'WRITE', 'XOR', 'YEAR_MONTH', 'ZEROFILL',
+		// New in MySQL 5.6.
+		'GET', 'IO_AFTER_GTIDS', 'IO_BEFORE_GTIDS, MASTER_BIND', 'ONE_SHOT', 'PARTITION', 'SQL_AFTER_GTIDS',
+		'SQL_BEFORE_GTIDS',
+		// New in MySQL 5.7.
+		'NONBLOCKING',
+	);
+
+	/**
 	 * All system columns that always are defined in a MetaModel table.
 	 *
 	 * When you alter this, ensure to also change @link{TableManipulation::STATEMENT_CREATE_TABLE} above.
@@ -93,6 +130,18 @@ class TableManipulation
 	}
 
 	/**
+	 * Test if the given word is a reserved MySQL word.
+	 *
+	 * @param string $word The word to test.
+	 *
+	 * @return bool
+	 */
+	public static function isReservedWord($word)
+	{
+		return in_array(strtoupper($word), self::$reservedWords);
+	}
+
+	/**
 	 * Ensure that an identifier is valid in MySQL context.
 	 *
 	 * @param string $strName The identifier to check.
@@ -103,7 +152,7 @@ class TableManipulation
 	{
 		// Match for valid table/column name, according to MySQL, a table name must start
 		// with a letter and must be combined of letters, decimals and underscore.
-		return 1 == preg_match('/^[a-z_][a-z\d_]*$/i', $strName);
+		return (1 == preg_match('/^[a-z_][a-z\d_]*$/i', $strName));
 	}
 
 	/**
@@ -115,7 +164,7 @@ class TableManipulation
 	 */
 	public static function isValidTablename($strTableName)
 	{
-		return self::isValidMySQLIdentifier($strTableName);
+		return self::isValidMySQLIdentifier($strTableName) && !self::isReservedWord($strTableName);
 	}
 
 	/**
@@ -127,7 +176,7 @@ class TableManipulation
 	 */
 	public static function isValidColumnName($strColName)
 	{
-		return self::isValidMySQLIdentifier($strColName);
+		return self::isValidMySQLIdentifier($strColName) && !self::isReservedWord($strColName);
 	}
 
 	/**
