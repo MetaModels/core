13057fb26099a82a9758e83b9bff78fcac731154
Mini Model <minimodel@metamodel.me>
2014-07-30 16:14:21 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/de/tl_metamodel_rendersetting.php
+++ src/system/modules/metamodels/languages/de/tl_metamodel_rendersetting.php
@@ -8,14 +8,13 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T22:11:20+02:00
+ * last-updated: 2013-07-10T10:02:32+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['row']                     = '%s <strong>%s</strong> <em>[%s]</em>';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addAll_addsuccess']       = 'Das Attribut "%s" wurde der Eingabemaske hinzugefügt.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addAll_alreadycontained'] = 'Das Attribut %s ist bereits in der Eingabemaske vorhanden.';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addAll_willadd']          = 'Das Attribut %s wird dem rendersetting hinzugefügt.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addAll_willadd']          = 'Das Attribut %s wird dem Rendersetting hinzugefügt.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addall']['0']             = 'Alle hinzufügen';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['addall']['1']             = 'Alle Attribute den Rendering Einstellungen hinzufügen';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['advanced_legend']         = 'Erweitern';
@@ -34,3 +33,4 @@ $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['show']['1']               = '
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['template']['0']           = 'Angepasstes Template für die Ausgabe';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['template']['1']           = 'Wählen Sie das Template aus, das für das gewählte Attribut verwendet werden soll. Gültige Dateinamen für das Template beginnen mit "mm_&lt;typ&gt;", wobei &lt;typ&gt; für den Typ steht.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['title_legend']            = 'Typ';
+
