1e1a32d1cda38c044dc61dabd9c3b865a99d4418
Tim Becker <tim@westwerk.ac>
2014-11-05 23:27:42 +0100

    cleanup, removed rendersetting and default, increase name size

--- contao/config/database.sql
+++ contao/config/database.sql
@@ -332,12 +332,10 @@ CREATE TABLE `tl_metamodel_searchable_pages` (
 
   `pid` int(10) unsigned NOT NULL default '0',
   `tstamp` int(10) unsigned NOT NULL default '0',
--- corresponding tl_metamodel_dca
-  `dca_settingId` int(10) unsigned NOT NULL default '0',
 -- active or disabled
   `enabled` char(1) NOT NULL default '',
--- human readable name of the searchable page setting for internal use only.
-  `name` varchar(64) NOT NULL default '',
+-- human readable description of the searchable page setting for internal use only.
+  `name` varchar(255) NOT NULL default '',
 -- corresponding tl_metamodel_filtersetting
   `filter_settingId` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
