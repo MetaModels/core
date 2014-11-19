0a86a6451294930a8789efc4bffea85d70ad472a
Stefan Heimes <stefan_heimes@hotmail.com>
2014-11-19 23:48:48 +0100

    Add the table events.
    Add the options events for the filter field and the render setting field.

---
+++ src/MetaModels/DcGeneral/Events/Table/SearchablePages/Filter.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\DcGeneral\Events\Table\SearchablePages;
+
+use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\GetPropertyOptionsEvent;
+
+/**
+ * Event handler to populate the options array of the filters.
+ *
+ * @package MetaModels\DcGeneral\Events\Table\SearchablePages
+ */
+class Filter
+{
+    /**
+     * Provide options for filter list.
+     *
+     * @param GetPropertyOptionsEvent $event The event.
+     *
+     * @return void
+     */
+    public static function getOptions(GetPropertyOptionsEvent $event)
+    {
+        $model = $event->getModel();
+        $pid   = $model->getProperty('pid');
+        if (empty($pid)) {
+            return;
+        }
+
+        $filter = \Database::getInstance()
+            ->prepare('SELECT id, name FROM tl_metamodel_filter WHERE pid=?')
+            ->execute($pid);
+
+        $options = array();
+        while ($filter->next()) {
+            $options[$filter->id] = $filter->name;
+        }
+
+        $event->setOptions($options);
+    }
+}
