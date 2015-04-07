e984590eb2bfdfb415b94802c544f705deaa53a7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-04-07 19:13:18 +0200

    Add missing breadcrumb class. Fixes #792.

---
+++ src/MetaModels/DcGeneral/Events/BreadCrumb/BreadCrumbDcaCombine.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\DcGeneral\Events\BreadCrumb;
+
+use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
+
+/**
+ * Generate a breadcrumb for table tl_metamodel_dcacombine.
+ *
+ * @package MetaModels\DcGeneral\Events\BreadCrumb
+ */
+class BreadCrumbDcaCombine extends BreadCrumbMetaModels
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getBreadcrumbElements(EnvironmentInterface $environment, $elements)
+    {
+        if (!isset($this->metamodelId)) {
+            $this->metamodelId = $this->extractIdFrom($environment, 'pid');
+        }
+
+        $elements   = parent::getBreadcrumbElements($environment, $elements);
+        $elements[] = array(
+            'url' => $this->generateUrl(
+                'tl_metamodel_dca_combine',
+                $this->seralizeId('tl_metamodel', $this->metamodelId)
+            ),
+            'text' => sprintf(
+                $this->getBreadcrumbLabel($environment, 'tl_metamodel_dca_combine'),
+                $this->getMetaModel()->getName()
+            ),
+            'icon' => $this->getBaseUrl() . '/system/modules/metamodels/assets/images/icons/dca_combine.png'
+        );
+
+        return $elements;
+    }
+}
