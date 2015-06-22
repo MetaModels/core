a3bd5ebc0d9930040df2917139c926c924cebf75
David Molineus <david.molineus@netzmacht.de>
2015-06-22 17:30:19 +0200

    Add author informations.

--- src/MetaModels/DcGeneral/Data/Driver.php
+++ src/MetaModels/DcGeneral/Data/Driver.php
@@ -12,6 +12,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Christopher Bölter <c.boelter@cogizz.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
--- src/MetaModels/DcGeneral/Events/MetaModel/PasteButton.php
+++ src/MetaModels/DcGeneral/Events/MetaModel/PasteButton.php
@@ -11,6 +11,7 @@
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
