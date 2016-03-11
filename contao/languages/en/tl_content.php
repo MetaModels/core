06980249f36c27ffbc54a21b17f9fb4df10d9ec2
xantippe <info@e-spin.de>
2016-03-11 22:44:09 +0100

    add author

--- contao/languages/en/tl_content.php
+++ contao/languages/en/tl_content.php
@@ -13,6 +13,7 @@
  * @package    MetaModels
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Ingolf Steinhardt <info@e-spin.de> 
  * @copyright  2012-2015 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
