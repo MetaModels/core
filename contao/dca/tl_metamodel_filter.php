271d1c3a2e1395209659f96989c872e798590ff2
Sven Baumann <baumann.sv@gmail.com>
2016-04-01 12:37:33 +0200

    add author

--- contao/dca/tl_metamodel_filter.php
+++ contao/dca/tl_metamodel_filter.php
@@ -16,6 +16,7 @@
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     David Maack <david.maack@arcor.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2015 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
