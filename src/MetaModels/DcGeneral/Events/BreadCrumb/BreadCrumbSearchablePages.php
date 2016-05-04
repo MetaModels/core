193da22189958326223f4a5f67f8035825183ff1
Ingolf Steinhardt <info@e-spin.de>
2016-05-04 09:08:09 +0200

    Hotfix at searchable pages: add breadcrumb

---
+++ src/MetaModels/DcGeneral/Events/BreadCrumb/BreadCrumbSearchablePages.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of MetaModels/core.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\DcGeneral\Events\BreadCrumb;
+
+use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
+
+/**
+ * Generate a breadcrumb for table tl_metamodel_searchable_pages.
+ */
+class BreadCrumbSearchablePages extends BreadCrumbMetaModels
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getBreadcrumbElements(EnvironmentInterface $environment, $elements)
+    {
+        if (!isset($this->metamodelId)) {
+            $this->metamodelId = $this->extractIdFrom($environment, 'pid');
+        }
+
+        $elements   = parent::getBreadcrumbElements($environment, $elements);
+        $elements[] = array(
+            'url' => $this->generateUrl(
+                'tl_metamodel_searchable_pages',
+                $this->seralizeId('tl_metamodel', $this->metamodelId),
+                true
+            ),
+            'text' => sprintf(
+                $this->getBreadcrumbLabel($environment, 'tl_metamodel_searchable_pages'),
+                $this->getMetaModel()->getName()
+            ),
+            'icon' => $this->getBaseUrl() . '/system/modules/metamodels/assets/images/icons/searchable_pages.png'
+        );
+
+        return $elements;
+    }
+}
