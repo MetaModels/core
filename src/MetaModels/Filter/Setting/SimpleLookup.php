b27a83d015ff989509eabbfcf32d654a98252549
Ingolf Steinhardt <info@e-spin.de>
2016-09-07 12:40:11 +0200

    Hotfix add only used by CE-MM-List at static param item select

--- src/MetaModels/Filter/Setting/SimpleLookup.php
+++ src/MetaModels/Filter/Setting/SimpleLookup.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2016 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     David Maack <david.maack@arcor.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2015 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -201,7 +202,8 @@ class SimpleLookup extends Simple
         }
 
         $objAttribute = $this->getFilteredAttribute();
-        $arrOptions   = $objAttribute->getFilterOptions(null, true);
+        $onlyUsed = $this->get('onlyused') ? true : false;
+        $arrOptions   = $objAttribute->getFilterOptions(null, $onlyUsed);
 
         return array(
             $this->getParamName() => array
