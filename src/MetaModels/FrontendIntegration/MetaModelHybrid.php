2116d8a9a1cae6a9cbe30adb3af574d5bfc86c0a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-11-17 23:12:11 +0100

    Add missing parameter to MetaModelHybrid

--- src/MetaModels/FrontendIntegration/MetaModelHybrid.php
+++ src/MetaModels/FrontendIntegration/MetaModelHybrid.php
@@ -20,6 +20,9 @@
 
 namespace MetaModels\FrontendIntegration;
 
+use Contao\ContentModel;
+use Contao\FormModel;
+use Contao\ModuleModel;
 use MetaModels\IMetaModelsServiceContainer;
 
 /**
@@ -64,11 +67,13 @@ abstract class MetaModelHybrid extends \Hybrid
     /**
      * Create a new instance.
      *
-     * @param \Database\Result $objElement The object from the database.
+     * @param ContentModel|ModuleModel|FormModel $objElement The object from the database.
+     *
+     * @param string                             $strColumn  The column the element is displayed within.
      */
-    public function __construct($objElement)
+    public function __construct($objElement, $strColumn = 'main')
     {
-        parent::__construct($objElement);
+        parent::__construct($objElement, $strColumn);
 
         $this->arrData = $objElement->row();
         // Get space and CSS ID from the parent element (!)
