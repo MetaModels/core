396fc63602b00c3e482dca18592ebbcfbbe22fd7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-11-18 01:36:40 +0100

    Split up boot classes
    We now have an abstract base class for booting tasks and concrete
    implementations for Frontend and Backend.

--- src/MetaModels/BackendIntegration/Boot.php
+++ src/MetaModels/BackendIntegration/Boot.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2016 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,30 +16,21 @@
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Andreas Nölke <zero@brothers-project.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2015 The MetaModels team.
+ * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
 namespace MetaModels\BackendIntegration;
 
-use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
-use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\LoadDataContainerEvent;
-use ContaoCommunityAlliance\Contao\Bindings\Events\System\LoadLanguageFileEvent;
-use ContaoCommunityAlliance\DcGeneral\Factory\Event\BuildDataDefinitionEvent;
-use ContaoCommunityAlliance\DcGeneral\Factory\Event\PopulateEnvironmentEvent;
-use ContaoCommunityAlliance\DcGeneral\Factory\Event\PreCreateDcGeneralEvent;
-use MetaModels\Dca\MetaModelDcaBuilder;
-use MetaModels\DcGeneral\Dca\Builder\Builder;
+use MetaModels\ContaoIntegration\Boot as BaseBoot;
 use MetaModels\DcGeneral\Events\Subscriber;
 use MetaModels\Events\MetaModelsBootEvent;
-use MetaModels\Helper\LoadDataContainerHookListener;
-use MetaModels\IMetaModelsServiceContainer;
 
 /**
  * This class is used in the backend to build the menu etc.
  */
-class Boot
+class Boot extends BaseBoot
 {
     /**
      * When we are within Contao >= 3.1, we have to override the file picker class.
@@ -56,43 +47,6 @@ class Boot
         ) {
             $GLOBALS['BE_FFL']['fileSelector'] = 'MetaModels\Widgets\FileSelectorWidget';
         }
-    }
-
-    /**
-     * Register the loadDataContainer HOOK for the given table name to create the DcGeneral etc.
-     *
-     * @param string                      $tableName The name of the table to be loaded.
-     *
-     * @param IMetaModelsServiceContainer $container The MetaModels service container.
-     *
-     * @return void
-     *
-     * @SuppressWarnings(PHPMD.Superglobals)
-     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
-     */
-    private function attachLoadDataContainerHook($tableName, $container)
-    {
-        LoadDataContainerHookListener::attachFor(
-            $tableName,
-            function ($tableName) use ($container) {
-                $dispatcher = $container->getEventDispatcher();
-                $event      = new LoadDataContainerEvent('tl_metamodel_item');
-                $dispatcher->dispatch(
-                    ContaoEvents::SYSTEM_LOAD_LANGUAGE_FILE,
-                    new LoadLanguageFileEvent('tl_metamodel_item')
-                );
-                $dispatcher->dispatch(ContaoEvents::CONTROLLER_LOAD_DATA_CONTAINER, $event);
-
-                if (!isset($GLOBALS['TL_DCA'][$tableName])) {
-                    $GLOBALS['TL_DCA'][$tableName] = array();
-                }
-
-                $GLOBALS['TL_DCA'][$tableName] = array_replace_recursive(
-                    (array) $GLOBALS['TL_DCA']['tl_metamodel_item'],
-                    (array) $GLOBALS['TL_DCA'][$tableName]
-                );
-            }
-        );
     }
 
     /**
@@ -115,66 +69,7 @@ class Boot
         $menuBuilder = new BackendModuleBuilder($container, $viewCombinations);
         $menuBuilder->export();
 
-        // Prepare lazy loading of the data containers.
-        foreach ($viewCombinations->getParentedInputScreenNames() as $metaModelName) {
-            $parent = $viewCombinations->getParentOf($metaModelName);
-            if (substr($parent, 0, 3) === 'mm_') {
-                continue;
-            }
-            LoadDataContainerHookListener::attachFor(
-                $parent,
-                function () use ($metaModelName, $viewCombinations, $container) {
-                    $inputScreen = $viewCombinations->getInputScreenDetails($metaModelName);
-                    $builder     = new MetaModelDcaBuilder($container);
-                    $builder->injectOperationButton($inputScreen);
-                }
-            );
-        }
-
-        $dispatcher = $container->getEventDispatcher();
-        foreach ($container->getFactory()->collectNames() as $metaModelName) {
-            $this->attachLoadDataContainerHook($metaModelName, $container);
-
-            $dispatcher->addListener(
-                PreCreateDcGeneralEvent::NAME,
-                function (PreCreateDcGeneralEvent $event) use ($metaModelName, $viewCombinations, $container) {
-                    $factory = $event->getFactory();
-                    $name    = $factory->getContainerName();
-                    if ($name !== $metaModelName) {
-                        return;
-                    }
-
-                    $inputScreen = $viewCombinations->getInputScreenDetails($metaModelName);
-
-                    $factory->setContainerClassName('MetaModels\DcGeneral\DataDefinition\MetaModelDataDefinition');
-
-                    $dispatcher = $container->getEventDispatcher();
-                    $generator  = new Builder($container, $inputScreen);
-
-                    $dispatcher->addListener(
-                        BuildDataDefinitionEvent::NAME,
-                        function (BuildDataDefinitionEvent $event) use ($metaModelName, $generator) {
-                            if ($event->getContainer()->getName() !== $metaModelName) {
-                                return;
-                            }
-                            $generator->build($event);
-                        },
-                        $generator::PRIORITY
-                    );
-                    $dispatcher->addListener(
-                        PopulateEnvironmentEvent::NAME,
-                        function (PopulateEnvironmentEvent $event) use ($metaModelName, $generator) {
-                            if ($event->getEnvironment()->getDataDefinition()->getName() !== $metaModelName) {
-                                return;
-                            }
-                            $generator->populate($event);
-                            $GLOBALS['TL_CSS'][] = 'system/modules/metamodels/assets/css/style.css';
-                        },
-                        $generator::PRIORITY
-                    );
-                }
-            );
-        }
+        $this->performBoot($container, $viewCombinations);
 
         // Register the global subscriber.
         new Subscriber($container);
---
+++ src/MetaModels/ContaoIntegration/Boot.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * This file is part of MetaModels/core.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\ContaoIntegration;
+
+use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
+use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\LoadDataContainerEvent;
+use ContaoCommunityAlliance\Contao\Bindings\Events\System\LoadLanguageFileEvent;
+use ContaoCommunityAlliance\DcGeneral\Factory\Event\BuildDataDefinitionEvent;
+use ContaoCommunityAlliance\DcGeneral\Factory\Event\PopulateEnvironmentEvent;
+use ContaoCommunityAlliance\DcGeneral\Factory\Event\PreCreateDcGeneralEvent;
+use MetaModels\Dca\MetaModelDcaBuilder;
+use MetaModels\DcGeneral\Dca\Builder\Builder;
+use MetaModels\DcGeneral\Events\Subscriber;
+use MetaModels\Helper\LoadDataContainerHookListener;
+use MetaModels\Helper\ViewCombinations;
+use MetaModels\IMetaModelsServiceContainer;
+
+/**
+ * This class registers the data container loaders.
+ *
+ * It is extended by the frontend and backend boot classes.
+ */
+abstract class Boot
+{
+    /**
+     * Register the loadDataContainer HOOK for the given table name to create the DcGeneral etc.
+     *
+     * @param string                      $tableName The name of the table to be loaded.
+     *
+     * @param IMetaModelsServiceContainer $container The MetaModels service container.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
+     */
+    private function attachLoadDataContainerHook($tableName, $container)
+    {
+        LoadDataContainerHookListener::attachFor(
+            $tableName,
+            function ($tableName) use ($container) {
+                $dispatcher = $container->getEventDispatcher();
+                $event      = new LoadDataContainerEvent('tl_metamodel_item');
+                $dispatcher->dispatch(
+                    ContaoEvents::SYSTEM_LOAD_LANGUAGE_FILE,
+                    new LoadLanguageFileEvent('tl_metamodel_item')
+                );
+                $dispatcher->dispatch(ContaoEvents::CONTROLLER_LOAD_DATA_CONTAINER, $event);
+
+                if (!isset($GLOBALS['TL_DCA'][$tableName])) {
+                    $GLOBALS['TL_DCA'][$tableName] = array();
+                }
+
+                $GLOBALS['TL_DCA'][$tableName] = array_replace_recursive(
+                    (array) $GLOBALS['TL_DCA']['tl_metamodel_item'],
+                    (array) $GLOBALS['TL_DCA'][$tableName]
+                );
+            }
+        );
+    }
+
+    /**
+     * Boot the system and register dca loader etc.
+     *
+     * @param IMetaModelsServiceContainer $container        The service container.
+     *
+     * @param ViewCombinations            $viewCombinations The view combinations to use.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
+     */
+    protected function performBoot(IMetaModelsServiceContainer $container, ViewCombinations $viewCombinations)
+    {
+        // Prepare lazy loading of the data containers.
+        foreach ($viewCombinations->getParentedInputScreenNames() as $metaModelName) {
+            $parent = $viewCombinations->getParentOf($metaModelName);
+            if (substr($parent, 0, 3) === 'mm_') {
+                continue;
+            }
+            LoadDataContainerHookListener::attachFor(
+                $parent,
+                function () use ($metaModelName, $viewCombinations, $container) {
+                    $inputScreen = $viewCombinations->getInputScreenDetails($metaModelName);
+                    $builder     = new MetaModelDcaBuilder($container);
+                    $builder->injectOperationButton($inputScreen);
+                }
+            );
+        }
+
+        $dispatcher = $container->getEventDispatcher();
+        foreach ($container->getFactory()->collectNames() as $metaModelName) {
+            $this->attachLoadDataContainerHook($metaModelName, $container);
+
+            $dispatcher->addListener(
+                PreCreateDcGeneralEvent::NAME,
+                function (PreCreateDcGeneralEvent $event) use ($metaModelName, $viewCombinations, $container) {
+                    $factory = $event->getFactory();
+                    $name    = $factory->getContainerName();
+                    if ($name !== $metaModelName) {
+                        return;
+                    }
+
+                    $inputScreen = $viewCombinations->getInputScreenDetails($metaModelName);
+
+                    $factory->setContainerClassName('MetaModels\DcGeneral\DataDefinition\MetaModelDataDefinition');
+
+                    $dispatcher = $container->getEventDispatcher();
+                    $generator  = new Builder($container, $inputScreen);
+
+                    $dispatcher->addListener(
+                        BuildDataDefinitionEvent::NAME,
+                        function (BuildDataDefinitionEvent $event) use ($metaModelName, $generator) {
+                            if ($event->getContainer()->getName() !== $metaModelName) {
+                                return;
+                            }
+                            $generator->build($event);
+                        },
+                        $generator::PRIORITY
+                    );
+                    $dispatcher->addListener(
+                        PopulateEnvironmentEvent::NAME,
+                        function (PopulateEnvironmentEvent $event) use ($metaModelName, $generator) {
+                            if ($event->getEnvironment()->getDataDefinition()->getName() !== $metaModelName) {
+                                return;
+                            }
+                            $generator->populate($event);
+                            $GLOBALS['TL_CSS'][] = 'system/modules/metamodels/assets/css/style.css';
+                        },
+                        $generator::PRIORITY
+                    );
+                }
+            );
+        }
+
+        // Register the global subscriber.
+        new Subscriber($container);
+    }
+}
