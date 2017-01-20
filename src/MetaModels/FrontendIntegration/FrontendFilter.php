2aa6cbc60f08bcc8100dedec51c72bd60ec07538
Ingolf Steinhardt <info@e-spin.de>
2017-01-20 16:02:18 +0100

    Hotfix filter_text filter value 0

--- src/MetaModels/FrontendIntegration/FrontendFilter.php
+++ src/MetaModels/FrontendIntegration/FrontendFilter.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2015 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -242,7 +243,7 @@ class FrontendFilter
         $redirectParameters = $allParameter['other'];
         foreach ($wantedParameter as $widgetName) {
             $filter = $widgets[$widgetName];
-            if (!empty($filter['urlvalue'])) {
+            if ($filter['urlvalue'] !== null) {
                 $redirectParameters[$widgetName] = $filter['urlvalue'];
             }
         }
