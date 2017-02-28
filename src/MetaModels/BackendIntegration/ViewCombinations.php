7932e3c9c4eea430f6bca0588f7fa2077f47609b
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-02-28 06:01:50 +0100

    Repair usage of scripts in system/bin
    This was in fact found and fixed by @sheeep from our fellows over at
    1up-lab, I simply stole it from his branch. :smile_cat:

--- src/MetaModels/BackendIntegration/ViewCombinations.php
+++ src/MetaModels/BackendIntegration/ViewCombinations.php
@@ -46,6 +46,10 @@ class ViewCombinations extends \MetaModels\Helper\ViewCombinations
             return false;
         }
 
+        if (strpos(Environment::get('script'), 'system/bin') !== false) {
+            return false;
+        }
+
         // Bug fix: If the user is not authenticated, contao will redirect to contao/index.php
         // But in this moment the TL_PATH is not defined, so the $this->Environment->request
         // generate a url without replacing the basepath(TL_PATH) with an empty string.
