3ab2f07c103756bb479d58aee56e8b420d267195
Danilo Benevides <danilobenevides01@gmail.com>
2017-08-06 19:44:46 -0300

    Update the copyright year to 2017.
    Change the copyright to 2017

--- src/MetaModels/Widgets/SubDcaWidget.php
+++ src/MetaModels/Widgets/SubDcaWidget.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Danilo Benevides <danilobenevides01@gmail.com>
- * @copyright  2012-2015 The MetaModels team.
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
