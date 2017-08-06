80ce00c0f256a7adacd17ec195d435fa4221c5cb
Danilo Benevides <danilobenevides01@gmail.com>
2017-08-06 09:23:09 -0300

    Add @author header.
    Add myself as @author on fle.

--- src/MetaModels/Widgets/SubDcaWidget.php
+++ src/MetaModels/Widgets/SubDcaWidget.php
@@ -14,6 +14,7 @@
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Danilo Benevides <danilobenevides01@gmail.com>
  * @copyright  2012-2015 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
