478a1dba35a85fb7669cc09811f806f8f534cbf5
Ingolf Steinhardt <info@e-spin.de>
2017-08-25 11:41:14 +0200

    hotfix usage inserttags in filter set idList

--- src/MetaModels/Filter/Setting/IdList.php
+++ src/MetaModels/Filter/Setting/IdList.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2015 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -36,7 +37,7 @@ class IdList extends Simple
     public function prepareRules(IFilter $objFilter, $arrFilterUrl)
     {
         if ($this->get('items')) {
-            $arrItems = explode(',', (string) $this->get('items'));
+            $arrItems = explode(',', (string) \Contao\Controller::replaceInsertTags($this->get('items'), false));
         } else {
             $arrItems = array();
         }
