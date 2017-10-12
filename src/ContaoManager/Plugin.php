80265880cb902aa3366220ed4924388a4fb1158c
Sven Baumann <baumann.sv@gmail.com>
2017-10-12 19:39:27 +0200

    From contao module to contao bundle, with support for the contao managed edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of MetaModels/core.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerBundle\ContaoManagerBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use MetaModels\MetaModelsCoreBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MetaModelsCoreBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        ContaoManagerBundle::class
+                    ]
+                )
+                ->setReplace(['metamodels'])
+        ];
+    }
+}
---
+++ src/MetaModelsCoreBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of MetaModels/core.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage Core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This class holds everything together.
+ */
+class MetaModelsCoreBundle extends Bundle
+{
+}
