5390ed709a96eb4d035b6e3a7d0c537e74218fd3
Sven Baumann <baumann.sv@gmail.com>
2017-10-12 19:55:59 +0200

    Define some services

--- contao/config/services.php
+++ contao/config/services.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2015 The MetaModels team.
+ * (c) 2012-2017 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,75 +13,54 @@
  * @package    MetaModels
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2015 The MetaModels team.
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2017 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
-/** @var Pimple $container */
+/** @var \DependencyInjection\Container\PimpleGate $container */
+$service = $container->getContainer();
 
-$container['metamodels-attribute-factory.factory'] = $container->share(
-    function () {
-        return new MetaModels\Attribute\AttributeFactory();
-    }
-);
+$container->provideSymfonyService('metamodels-attribute-factory.factory');
+$container->provideSymfonyService('metamodels-factory.factory');
+$container->provideSymfonyService('metamodels-filter-setting-factory.factory');
+$container->provideSymfonyService('metamodels-render-setting-factory.factory');
 
-$container['metamodels-factory.factory'] = $container->share(
-    function () {
-        return new MetaModels\Factory();
-    }
-);
-
-$container['metamodels-filter-setting-factory.factory'] = $container->share(
-    function () {
-        return new MetaModels\Filter\Setting\FilterSettingFactory();
-    }
-);
-
-if (!isset($container['metamodels-filter-setting-factory.factory'])) {
-    $container['metamodels-filter-setting-factory.factory'] =
-        $container->raw('metamodels-filter-setting-factory.factory.default');
+// Fixme what cache system we use?
+$cacheFactory = null;
+if ($container['config']->get('bypassCache')) {
+    $cacheFactory = new \Doctrine\Common\Cache\ArrayCache();
+} else {
+    $cacheFactory = new \Doctrine\Common\Cache\FilesystemCache(TL_ROOT . '/system/cache/metamodels');
 }
-
-$container['metamodels-render-setting-factory.factory'] = $container->share(
-    function () {
-        return new MetaModels\Render\Setting\RenderSettingFactory();
-    }
+$container->provideSymfonyService('metamodels-cache.factory');
+$service->set(
+    'metamodels-cache.factory',
+    $cacheFactory
 );
 
-$container['metamodels-cache.factory'] = $container->share(
-    function ($container) {
-        if ($container['config']->get('bypassCache')) {
-            return new \Doctrine\Common\Cache\ArrayCache();
-        }
+// Fixme build an factory for metamodels service container.
+$serviceContainer = new MetaModels\MetaModelsServiceContainer();
+$dispatcher       = $container['event-dispatcher'];
+$serviceContainer
+    ->setEventDispatcher($dispatcher)
+    ->setDatabase($container['database.connection']);
 
-        return new \Doctrine\Common\Cache\FilesystemCache(TL_ROOT . '/system/cache/metamodels');
-    }
+$serviceContainer
+    ->setAttributeFactory($container['metamodels-attribute-factory.factory'])
+    ->setFactory($container['metamodels-factory.factory'])
+    ->setFilterFactory($container['metamodels-filter-setting-factory.factory'])
+    ->setRenderSettingFactory($container['metamodels-render-setting-factory.factory'])
+    ->setCache($container['metamodels-cache.factory']);
+$container->provideSymfonyService('metamodels-service-container.factory');
+$service->set(
+    'metamodels-service-container.factory',
+    $serviceContainer
 );
 
-$container['metamodels-service-container.factory'] = $container->share(
-    function ($container) {
-        $serviceContainer = new MetaModels\MetaModelsServiceContainer();
-        $dispatcher       = $container['event-dispatcher'];
-        $serviceContainer
-            ->setEventDispatcher($dispatcher)
-            ->setDatabase($container['database.connection']);
-
-        $serviceContainer
-            ->setAttributeFactory($container['metamodels-attribute-factory.factory'])
-            ->setFactory($container['metamodels-factory.factory'])
-            ->setFilterFactory($container['metamodels-filter-setting-factory.factory'])
-            ->setRenderSettingFactory($container['metamodels-render-setting-factory.factory'])
-            ->setCache($container['metamodels-cache.factory']);
-
-        return $serviceContainer;
-    }
-);
-
-$container['metamodels-service-container'] = $container->share(
-    function ($container) {
-        $factory = $container['metamodels-service-container.factory'];
-
-        return $factory;
-    }
+$container->provideSymfonyService('metamodels-service-container');
+$service->set(
+    'metamodels-service-container',
+    $service->get('metamodels-service-container.factory')
 );
---
+++ src/DependencyInjection/MetaModelsCoreExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general.
+ *
+ * (c) 2013-2017 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Andreas Isaak <andy.jared@googlemail.com>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @copyright  2013-2017 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages the bundle configuration
+ */
+class MetaModelsCoreExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
