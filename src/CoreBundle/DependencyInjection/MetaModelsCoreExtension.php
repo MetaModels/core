62ead03c718d4406bb502ca468e2e7c2abbff856
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-10-18 22:24:45 +0200

    Extract filter setting factories into own yml

--- src/CoreBundle/DependencyInjection/MetaModelsCoreExtension.php
+++ src/CoreBundle/DependencyInjection/MetaModelsCoreExtension.php
@@ -41,5 +41,6 @@ class MetaModelsCoreExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('listeners.yml');
+        $loader->load('filter-settings.yml');
     }
 }
