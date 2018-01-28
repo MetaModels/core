226542768afa578cad9a613a51accca21e60c57e
Sven Baumann <baumann.sv@gmail.com>
2018-01-28 04:36:01 +0100

    Restores the functionality of the stylepicker in dca setting

--- src/CoreBundle/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/CoreBundle/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2018 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -21,7 +21,7 @@
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2017 The MetaModels team.
+ * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -363,7 +363,7 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting'] = array
             'inputType' => 'text',
             'eval'      => array
             (
-                'tl_class' => 'long wizard',
+                'tl_class' => 'wizard',
             ),
             'sql'       => "varchar(64) NOT NULL default ''"
         ),
