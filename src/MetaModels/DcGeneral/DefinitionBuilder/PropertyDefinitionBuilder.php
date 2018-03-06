928c09e244d06155b2e05ac8a8d5ada5cef2c4cb
Christian Schiffler <c.schiffler@cyberspectrum.de>
2018-03-06 02:36:56 +0100

    Remove dead code related to #1141

--- src/MetaModels/DcGeneral/DefinitionBuilder/PropertyDefinitionBuilder.php
+++ src/MetaModels/DcGeneral/DefinitionBuilder/PropertyDefinitionBuilder.php
@@ -88,21 +88,9 @@ class PropertyDefinitionBuilder
 
         // If the current metamodels has variants add the varbase and vargroup to the definition.
         if ($metaModel->hasVariants()) {
-            // FIXME: these are not properties yet, therefore we have to work around.
+            // These are not properties yet, therefore we have to work around.
             $this->getOrCreateProperty($definition, 'varbase');
             $this->getOrCreateProperty($definition, 'vargroup');
-            /*
-            $this->buildProperty(
-                $definition,
-                $metaModel->getAttribute('varbase'),
-                $inputScreen->getProperty('varbase')['info']
-            );
-            $this->buildProperty(
-                $definition,
-                $metaModel->getAttribute('vargroup'),
-                $inputScreen->getProperty('vargroup')['info']
-            );
-            */
         }
 
         foreach ($metaModel->getAttributes() as $attribute) {
