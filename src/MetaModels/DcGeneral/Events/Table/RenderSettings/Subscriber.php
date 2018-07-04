1945b8d25080cd00a70083cc88ffc26288750357
Christian Schiffler <c.schiffler@cyberspectrum.de>
2018-07-04 19:31:54 +0200

    Fix author validation
    The upgrade to phpcq/author-validation 1.1.6 fixes a bug in the author
    validation and now finds more authors that were missing before.

--- src/MetaModels/DcGeneral/Events/Table/RenderSettings/Subscriber.php
+++ src/MetaModels/DcGeneral/Events/Table/RenderSettings/Subscriber.php
@@ -17,6 +17,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright  2012-2018 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
