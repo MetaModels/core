44ef018a9ebe61b9a94223b28325de031d4ead58
Mini Model <minimodel@metamodel.me>
2018-12-13 02:55:53 +0100

    Update PHPDoc file headers

--- contao/dca/tl_metamodel.php
+++ contao/dca/tl_metamodel.php
@@ -10,8 +10,7 @@
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage Core
+ * @package    MetaModels/core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Christian de la Haye <service@delahaye.de>
  * @author     Andreas Isaak <info@andreas-isaak.de>
--- src/MetaModels/Helper/TableManipulation.php
+++ src/MetaModels/Helper/TableManipulation.php
@@ -10,8 +10,7 @@
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage Core
+ * @package    MetaModels/core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Henry Lamorski <henry.lamorski@mailbox.org>
  * @author     Sven Baumann <baumann.sv@gmail.com>
