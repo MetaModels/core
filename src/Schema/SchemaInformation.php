cba7000912e570c80fafb54e7cc63b89038a1534
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-29 23:17:38 +0100

    Initialize array

--- src/Schema/SchemaInformation.php
+++ src/Schema/SchemaInformation.php
@@ -31,7 +31,7 @@ class SchemaInformation
      *
      * @var SchemaInformationInterface[]
      */
-    private $information;
+    private $information = [];
 
     /**
      * Retrieve schema information.
