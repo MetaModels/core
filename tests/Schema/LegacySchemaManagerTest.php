248e0620e2bc2267e74253714cee2e3f254f2a7d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2019-01-30 04:02:39 +0100

    Add legacy schema manager

---
+++ tests/Schema/LegacySchemaManagerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of MetaModels/core.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/core
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types = 1);
+
+namespace MetaModels\Test\Schema\Doctrine;
+
+use MetaModels\Attribute\IAttribute;
+use MetaModels\Attribute\IComplex;
+use MetaModels\Attribute\ISimple;
+use MetaModels\Schema\LegacySchemaInformation;
+use MetaModels\Schema\LegacySchemaManager;
+use MetaModels\Schema\SchemaInformation;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This tests the doctrine schema.
+ *
+ * @covers \MetaModels\Schema\LegacySchemaManager
+ */
+class LegacySchemaManagerTest extends TestCase
+{
+    /**
+     * Test the instantiation.
+     *
+     * @return void
+     */
+    public function testInstantiation(): void
+    {
+        $instance = new LegacySchemaManager();
+
+        $this->assertInstanceOf(LegacySchemaManager::class, $instance);
+    }
+
+    /**
+     * Test the generate method.
+     *
+     * @return void
+     */
+    public function testIgnoresIfNotFound(): void
+    {
+        $instance    = new LegacySchemaManager();
+        $information = $this->getMockBuilder(SchemaInformation::class)->getMock();
+        $information->expects($this->once())->method('has')->with(LegacySchemaInformation::class)->willReturn(false);
+        $information->expects($this->never())->method('get');
+
+        $instance->process($information);
+    }
+
+    /**
+     * Test the process method.
+     *
+     * @return void
+     */
+    public function testProcess(): void
+    {
+        $information = new SchemaInformation();
+        $legacy      = new LegacySchemaInformation();
+        $information->add($legacy);
+
+        $legacy->addAttribute($attribute1 = $this->getMockForAbstractClass(ISimple::class));
+        $legacy->addAttribute($attribute2 = $this->getMockForAbstractClass(IComplex::class));
+        $legacy->addAttribute($attribute3 = $this->getMockForAbstractClass(IAttribute::class));
+
+        $attribute1->expects($this->once())->method('initializeAUX');
+        $attribute2->expects($this->once())->method('initializeAUX');
+        $attribute3->expects($this->once())->method('initializeAUX')->willThrowException(new \RuntimeException());
+
+        $instance = new LegacySchemaManager();
+
+        $instance->process($information);
+    }
+}
