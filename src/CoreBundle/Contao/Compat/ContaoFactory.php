e1c44e3b95d4384d527b8a77e3978c2b7aa41c48
Stefan Heimes <stefan_heimes@hotmail.com>
2019-05-16 23:23:53 +0200

    Add missing authors.

--- src/CoreBundle/Contao/Compat/ContaoFactory.php
+++ src/CoreBundle/Contao/Compat/ContaoFactory.php
@@ -12,6 +12,8 @@
  *
  * @package    MetaModels/core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     binron <rtb@gmx.ch>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
