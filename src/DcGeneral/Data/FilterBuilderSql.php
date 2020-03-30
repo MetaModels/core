4705216fa8c14937e28cf92a14c442ead4757b3f
e-spin <gitlab@e-spin.de>
2020-03-30 10:36:19 +0200

    check all select, update, insert, delete

--- src/DcGeneral/Data/FilterBuilderSql.php
+++ src/DcGeneral/Data/FilterBuilderSql.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -121,7 +122,7 @@ class FilterBuilderSql
     {
         if (!$this->isEmpty()) {
             return new SimpleQuery(
-                sprintf('SELECT id FROM %s WHERE %s', $this->tableName, $this->getProcedure()),
+                sprintf('SELECT id FROM `%s` WHERE `%s`', $this->tableName, $this->getProcedure()),
                 $this->getParameters(),
                 'id',
                 $this->connection
