5fc5fede854ba609ab7c1471df82150f36b8415f
Marc Reimann <reimann@mediendepot-ruhr.de>
2020-07-17 08:55:10 +0200

    Update BaseSimple.php
    Fix for #1412

--- src/Attribute/BaseSimple.php
+++ src/Attribute/BaseSimple.php
@@ -162,8 +162,7 @@ class BaseSimple extends Base implements ISimple
                 ->select($strCol . ', COUNT(' . $strCol . ') as mm_count')
                 ->from($this->getMetaModel()->getTableName())
                 ->where('id IN (:ids)')
-                ->groupBy('id')
-                ->addGroupBy($strCol)
+                ->groupBy('$strCol')
                 ->orderBy('FIELD(id, :ids)')
                 ->setParameter('ids', $idList, Connection::PARAM_STR_ARRAY)
                 ->execute();
@@ -171,8 +170,7 @@ class BaseSimple extends Base implements ISimple
             $statement = $this->connection->createQueryBuilder()
                 ->select($strCol . ', COUNT(' . $strCol . ') as mm_count')
                 ->from($this->getMetaModel()->getTableName())
-                ->groupBy('id')
-                ->addGroupBy($strCol)
+                ->groupBy('$strCol')
                 ->orderBy($strCol)
                 ->execute();
         }
