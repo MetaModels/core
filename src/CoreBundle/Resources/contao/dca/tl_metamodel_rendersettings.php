c49a537dfcfbc237a0e079c35ed0025e5d262639
Cliff Parnitzky <github@cliff-parnitzky.de>
2020-10-14 00:30:38 +0200

    Fix different max length definition of DCA name fields in SQL and eval array

--- src/CoreBundle/Resources/contao/dca/tl_metamodel_rendersettings.php
+++ src/CoreBundle/Resources/contao/dca/tl_metamodel_rendersettings.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -20,7 +20,8 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -239,7 +240,7 @@ $GLOBALS['TL_DCA']['tl_metamodel_rendersettings'] = array
                 'maxlength' => 255,
                 'tl_class'  => 'w50'
             ),
-            'sql'       => "varchar(64) NOT NULL default ''"
+            'sql'       => "varchar(255) NOT NULL default ''"
         ),
         'hideEmptyValues' => array
         (
