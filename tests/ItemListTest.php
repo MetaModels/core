7962b0eb190466440e744ce2874f4e3468dcb7b7
Sven Baumann <baumann.sv@gmail.com>
2021-04-12 19:19:19 +0200

    Fix author-validation issue

--- tests/ItemListTest.php
+++ tests/ItemListTest.php
@@ -12,6 +12,7 @@
  *
  * @package    MetaModels/core
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
