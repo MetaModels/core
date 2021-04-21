0ebfad03e19fe67424100a870922752e77749355
Christian Schiffler <c.schiffler@cyberspectrum.de>
2021-04-21 16:37:11 +0200

    Reduce amount of data to obtain for search urls
    Before, all properties from the MetaModel were loaded, this now limits
    the amount to the defined attributes in the render setting.

--- src/CoreBundle/EventListener/GetSearchablePagesListener.php
+++ src/CoreBundle/EventListener/GetSearchablePagesListener.php
@@ -293,7 +293,7 @@ class GetSearchablePagesListener implements ServiceAnnotationInterface
 
         // Get the object.
         /** @var Item $item */
-        foreach ($metaModels->findByFilter($filter) as $item) {
+        foreach ($metaModels->findByFilter($filter, '', 0, 0, 'ASC', $view->getSettingNames()) as $item) {
             $jumpTo = $item->buildJumpToLink($view);
             $event  = new GetPageDetailsEvent($jumpTo['page']);
             $this->dispatcher->dispatch($event, ContaoEvents::CONTROLLER_GET_PAGE_DETAILS);
