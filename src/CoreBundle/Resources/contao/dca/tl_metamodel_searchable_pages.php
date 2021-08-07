4c8844b14f601f168caa09a2f6c5f8e979c7a14f
Ingolf Steinhardt <info@e-spin.de>
2021-08-07 11:51:49 +0200

    Set indexing default to published

--- src/CoreBundle/Resources/contao/dca/tl_metamodel_searchable_pages.php
+++ src/CoreBundle/Resources/contao/dca/tl_metamodel_searchable_pages.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2020 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2020 The MetaModels team.
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -197,8 +197,9 @@ $GLOBALS['TL_DCA']['tl_metamodel_searchable_pages'] = [
             ],
             'sql'       => "int(10) unsigned NOT NULL default '0'"
         ],
-        'published'     => [
-            'sql' => "char(1) NOT NULL default ''"
+        'published' => [
+            'default' => 1,
+            'sql'     => "char(1) NOT NULL default '1'"
         ]
     ]
 ];
