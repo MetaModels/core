d396890c0139a6189535f123d53a161437bba9f6
Ingolf Steinhardt <git@e-spin.de>
2022-05-11 14:54:43 +0200

    Add inverse dca setting to show parent edit icon

--- src/CoreBundle/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/CoreBundle/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -21,7 +21,7 @@
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -85,6 +85,13 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting'] = [
                     ],
                 ],
                 'filter'  => [
+                    [
+                        'local'     => 'pid',
+                        'remote'    => 'id',
+                        'operation' => '=',
+                    ],
+                ],
+                'inverse' => [
                     [
                         'local'     => 'pid',
                         'remote'    => 'id',
