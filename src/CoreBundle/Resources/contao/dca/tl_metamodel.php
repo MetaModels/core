ffac7cfcf0b3df2e73b8b38875aff5a19c761d69
Ingolf Steinhardt <git@e-spin.de>
2022-05-12 17:01:28 +0200

    Delete submitonchange at varsupport

--- src/CoreBundle/Resources/contao/dca/tl_metamodel.php
+++ src/CoreBundle/Resources/contao/dca/tl_metamodel.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2020 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -21,7 +21,7 @@
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
- * @copyright  2012-2020 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -498,8 +498,7 @@ $GLOBALS['TL_DCA']['tl_metamodel'] = [
             'exclude'   => true,
             'inputType' => 'checkbox',
             'eval'      => [
-                'tl_class'       => 'clr',
-                'submitOnChange' => true
+                'tl_class'       => 'clr w50'
             ],
             'sql'       => "char(1) NOT NULL default ''"
         ],
