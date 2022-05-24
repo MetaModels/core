b2f312903c798e9f35d1de14f03adf3c336d65a2
Ingolf Steinhardt <info@e-spin.de>
2022-05-24 14:17:20 +0200

    Fix symfony warning

--- src/Render/Setting/Collection.php
+++ src/Render/Setting/Collection.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -151,7 +151,7 @@ class Collection implements ICollection
      */
     public function get($strName)
     {
-        return $this->arrBase[$strName];
+        return !isset($this->arrBase[$strName]) ?? $this->arrBase[$strName];
     }
 
     /**
