bd84616d0bb13d55248fa1cdc5c39713debdd988
Ingolf Steinhardt <info@e-spin.de>
2022-05-25 22:19:46 +0200

    Add change for Xtra fix in DCG

--- src/CoreBundle/Resources/contao/languages/en/tl_metamodel_dca_combine.php
+++ src/CoreBundle/Resources/contao/languages/en/tl_metamodel_dca_combine.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2020 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,10 +15,12 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2020 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
+
+$GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_combiner_legend'] = 'Combination configuration';
 
 $GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_combiner'][0] = 'Permissions for input screen and views';
 $GLOBALS['TL_LANG']['tl_metamodel_dca_combine']['dca_combiner'][1] =
