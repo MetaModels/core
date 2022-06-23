0595f82f074de2b9777c1efc849cd51350a3851e
Stefan Heimes <stefan_heimes@hotmail.com>
2022-06-23 23:15:04 +0200

    Correct author

--- src/Filter/FilterUrlBuilder.php
+++ src/Filter/FilterUrlBuilder.php
@@ -13,6 +13,7 @@
  * @package    MetaModels/core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
