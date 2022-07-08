75e3f4010b2125f3173571a30f97ad02805dad47
Ingolf Steinhardt <info@e-spin.de>
2022-07-08 23:37:31 +0200

    Fix reserved words

--- src/Attribute/BaseSimple.php
+++ src/Attribute/BaseSimple.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,7 +18,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Marc Reimann <reimann@mediendepot-ruhr.de>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -166,7 +166,7 @@ class BaseSimple extends Base implements ISimple
                 ->select('t.' . $strCol . ', COUNT(t.' . $strCol . ') as mm_count')
                 ->from($this->getMetaModel()->getTableName(), 't')
                 ->where('t.id IN (:ids)')
-                ->groupBy($strCol)
+                ->groupBy('t.' . $strCol)
                 ->orderBy('MIN(FIELD(t.id, :ids))')
                 ->setParameter('ids', $idList, Connection::PARAM_STR_ARRAY)
                 ->execute();
