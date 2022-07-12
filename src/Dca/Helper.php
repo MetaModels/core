40d7358b6c505af9f5fb60881e5bcfc50695c656
Ingolf Steinhardt <info@e-spin.de>
2022-07-12 18:09:23 +0200

    Fix PHP 8 warnings

--- src/Dca/Helper.php
+++ src/Dca/Helper.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2020 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -19,7 +19,7 @@
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2020 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -41,7 +41,6 @@ class Helper
      * Decode a language array.
      *
      * @param array|string $varValue     The value to decode.
-     *
      * @param IMetaModel   $objMetaModel The MetaModel holding the languages.
      *
      * @return string
@@ -63,7 +62,7 @@ class Helper
         if ($arrLanguages) {
             foreach ($arrLanguages as $strLangCode) {
                 if (is_array($arrLangValues)) {
-                    $varSubValue = $arrLangValues[$strLangCode];
+                    $varSubValue = $arrLangValues[$strLangCode] ?? '';
                 } else {
                     $varSubValue = $arrLangValues;
                 }
