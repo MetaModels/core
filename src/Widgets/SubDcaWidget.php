e0b54b57d9b6f6211140977fbfc9a43fbd6d5776
Ingolf Steinhardt <info@e-spin.de>
2022-10-06 14:49:53 +0200

    Fix typesave

--- src/Widgets/SubDcaWidget.php
+++ src/Widgets/SubDcaWidget.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Cliff Parnitzky <github@cliff-parnitzky.de>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -306,7 +306,7 @@ class SubDcaWidget extends Widget
             $arrField,
             $arrField['name'],
             $arrField['value'],
-            null,
+            '',
             $this->strTable,
             $this->objDca
         );
