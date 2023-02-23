f34af64fd1e690f27f84fc3c99b67445a80491ce
e-spin <gitlab@e-spin.de>
2023-02-23 12:44:11 +0100

    Fix PHP8 warning

--- src/CoreBundle/Contao/Hooks/AbstractContentElementAndModuleCallback.php
+++ src/CoreBundle/Contao/Hooks/AbstractContentElementAndModuleCallback.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2022 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,7 @@
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Marc Reimann <reimann@mediendepot-ruhr.de>
- * @copyright  2012-2022 The MetaModels team.
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -350,7 +350,7 @@ abstract class AbstractContentElementAndModuleCallback
         $dca        = $collection->getParameterDCA();
         foreach ($dca as $fieldName => $subField) {
             $options = [];
-            foreach ($subField['options'] as $key => $value) {
+            foreach (($subField['options'] ?? []) as $key => $value) {
                 $options[$this->loadCallback($key)] = $value;
             }
 
