dba30675df5dcf72cafbcc2663cbee54de0558cd
e-spin <gitlab@e-spin.de>
2023-04-12 14:05:37 +0200

    Fix create variants

--- src/CoreBundle/EventListener/DcGeneral/MetaModel/CreateVariantButtonListener.php
+++ src/CoreBundle/EventListener/DcGeneral/MetaModel/CreateVariantButtonListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,8 @@
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -146,7 +147,7 @@ class CreateVariantButtonListener
         $postFunction = function ($environment, $model) {
             /** @var EnvironmentInterface $environment */
             $copyEvent = new PostCreateModelEvent($environment, $model);
-            $environment->getEventDispatcher()->dispatch($copyEvent::NAME, $copyEvent);
+            $environment->getEventDispatcher()->dispatch($copyEvent, $copyEvent::NAME);
         };
 
         if (!$view instanceof BackendViewInterface) {
