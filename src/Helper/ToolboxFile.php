5797d8de261ad896a544d16a0b3490c56134fae5
Ingolf Steinhardt <info@e-spin.de>
2024-10-29 09:26:08 +0100

    Fix typo

--- src/Helper/ToolboxFile.php
+++ src/Helper/ToolboxFile.php
@@ -51,6 +51,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
 
+use Throwable;
 use function array_fill;
 use function array_filter;
 use function array_flip;
@@ -292,7 +293,7 @@ class ToolboxFile
             );
             // @codingStandardsIgnoreEnd
             $rootDir = System::getContainer()->getParameter('kernel.project_dir');
-            assert(\is_string($rootDir));
+            assert(is_string($rootDir));
         }
         $this->rootDir = $rootDir;
 
@@ -349,11 +350,11 @@ class ToolboxFile
         // We must not allow file extensions that are globally disabled.
         $allowedDownload = StringUtil::trimsplit(',', $GLOBALS['TL_CONFIG']['allowedDownload']);
 
-        if (!\is_array($acceptedExtensions)) {
+        if (!is_array($acceptedExtensions)) {
             $acceptedExtensions = StringUtil::trimsplit(',', $acceptedExtensions);
         }
 
-        $this->acceptedExtensions = \array_map('strtolower', \array_intersect($allowedDownload, $acceptedExtensions));
+        $this->acceptedExtensions = array_map('strtolower', array_intersect($allowedDownload, $acceptedExtensions));
     }
 
     /**
@@ -555,10 +556,10 @@ class ToolboxFile
 
         $conditions = [];
         $parameters = [];
-        if (\count($this->pendingIds)) {
+        if (count($this->pendingIds)) {
             $conditions[] = $table . '.uuid IN(' .
-                            \implode(',', \array_fill(0, \count($this->pendingIds), 'UNHEX(?)')) . ')';
-            $parameters   = \array_map('bin2hex', $this->pendingIds);
+                            implode(',', array_fill(0, count($this->pendingIds), 'UNHEX(?)')) . ')';
+            $parameters   = array_map('bin2hex', $this->pendingIds);
 
             $this->pendingIds = [];
         }
@@ -571,11 +572,11 @@ class ToolboxFile
             $this->pendingPaths = [];
         }
 
-        if (!\count($conditions)) {
+        if (!count($conditions)) {
             return;
         }
 
-        $files = FilesModel::findBy([\implode(' OR ', $conditions)], $parameters);
+        $files = FilesModel::findBy([implode(' OR ', $conditions)], $parameters);
         if ($files instanceof Collection) {
             $this->addFileModels($files);
         }
@@ -601,7 +602,7 @@ class ToolboxFile
     {
         if (!$this->withDownloadKeys) {
             return UrlBuilder::fromUrl(Environment::get('request'))
-                ->setQueryParameter('file', \urlencode($strFile))
+                ->setQueryParameter('file', urlencode($strFile))
                 ->getUrl();
         }
         // Throws exception when running in CLI mode due to missing session.
@@ -609,11 +610,11 @@ class ToolboxFile
         assert($bag instanceof AttributeBagInterface);
 
         $links = $bag->has('metaModels_downloads') ? $bag->get('metaModels_downloads') : [];
-        if (!\is_array($links)) {
+        if (!is_array($links)) {
             $links = [];
         }
         if (!isset($links[$strFile])) {
-            $links[$strFile] = \md5(\uniqid('', true));
+            $links[$strFile] = md5(uniqid('', true));
             $bag->set('metaModels_downloads', $links);
         }
 
@@ -664,7 +665,7 @@ class ToolboxFile
         $files  = [];
         $source = [];
 
-        foreach (\array_keys($arrFiles) as $k) {
+        foreach (array_keys($arrFiles) as $k) {
             $files[]  = $arrFiles[$k];
             $source[] = $arrSource[$k];
         }
@@ -727,8 +728,8 @@ class ToolboxFile
      */
     protected function addClasses(&$arrSource)
     {
-        $countFiles = \count($arrSource);
-        foreach (\array_keys($arrSource) as $k) {
+        $countFiles = count($arrSource);
+        foreach (array_keys($arrSource) as $k) {
             $arrSource[$k]['class'] = (($k === 0) ? ' first' : '') .
                                       (($k === ($countFiles - 1)) ? ' last' : '') .
                                       ((($k % 2) === 0) ? ' even' : ' odd');
@@ -750,7 +751,7 @@ class ToolboxFile
             return ['files' => [], 'source' => []];
         }
 
-        \uasort($arrFiles, ($blnAscending) ? '\basename_natcasecmp' : '\basename_natcasercmp');
+        uasort($arrFiles, [$this, ($blnAscending) ? 'basenameNatcasecmp' : 'basenameNatcasercmp']);
 
         return $this->remapSorting($arrFiles, $this->outputBuffer);
     }
@@ -772,9 +773,9 @@ class ToolboxFile
         }
 
         if ($blnAscending) {
-            \array_multisort($arrFiles, SORT_NUMERIC, $arrDates, SORT_ASC);
+            array_multisort($arrFiles, SORT_NUMERIC, $arrDates, SORT_ASC);
         } else {
-            \array_multisort($arrFiles, SORT_NUMERIC, $arrDates, SORT_DESC);
+            array_multisort($arrFiles, SORT_NUMERIC, $arrDates, SORT_DESC);
         }
 
         return $this->remapSorting($arrFiles, $this->outputBuffer);
@@ -793,7 +794,7 @@ class ToolboxFile
         if (!$fileMap) {
             return ['files' => [], 'source' => []];
         }
-        $fileKeys = \array_flip(\array_keys($this->uuidMap));
+        $fileKeys = array_flip(array_keys($this->uuidMap));
         $sorted   = [];
         foreach ($sortIds as $sortStringId) {
             $key          = $fileKeys[$sortStringId];
@@ -820,9 +821,9 @@ class ToolboxFile
             return ['files' => [], 'source' => []];
         }
 
-        $keys  = \array_keys($arrFiles);
+        $keys  = array_keys($arrFiles);
         $files = [];
-        \shuffle($keys);
+        shuffle($keys);
         foreach ($keys as $key) {
             $files[$key] = $arrFiles[$key];
         }
@@ -886,7 +887,7 @@ class ToolboxFile
                 assert($bag instanceof AttributeBagInterface);
                 $links = $bag->has('metaModels_downloads') ? $bag->get('metaModels_downloads') : [];
 
-                if (!\is_array($links)) {
+                if (!is_array($links)) {
                     $links = [];
                 }
                 // Check key and return 403 if mismatch
@@ -977,13 +978,13 @@ class ToolboxFile
     public static function convertValuesToMetaModels($values)
     {
         /** @psalm-suppress DocblockTypeContradiction */
-        if (!\is_array($values)) {
+        if (!is_array($values)) {
             throw new InvalidArgumentException('Invalid uuid list.');
         }
 
         // Convert UUIDs to binary and clean empty values out.
-        $values = \array_filter(
-            \array_map(function ($fileId) {
+        $values = array_filter(
+            array_map(function ($fileId) {
                 return Validator::isStringUuid($fileId) ? StringUtil::uuidToBin($fileId) : $fileId;
             }, $values)
         );
@@ -1033,7 +1034,7 @@ class ToolboxFile
      */
     public static function convertUuidsOrPathsToMetaModels($values)
     {
-        $values = \array_filter($values);
+        $values = array_filter($values);
         if (empty($values)) {
             return [
                 'bin'   => [],
@@ -1045,7 +1046,7 @@ class ToolboxFile
 
         foreach ($values as $key => $value) {
             if (!(Validator::isUuid($value))) {
-                if (!\is_string($value)) {
+                if (!is_string($value)) {
                     continue;
                 }
 
@@ -1147,7 +1148,7 @@ class ToolboxFile
         if ($information['isGdImage'] = $file->isGdImage) {
             try {
                 $information['src'] = urldecode($this->resizeImage($fileName));
-            } catch (\Throwable $exception) {
+            } catch (Throwable $exception) {
                 // Broken image, keep original path.
                 $information['src'] = urldecode($fileName);
                 $information['isGdImage'] = false;
@@ -1198,7 +1199,7 @@ class ToolboxFile
                     $information['lightboxPicture'] = $lightboxPicture;
                     $information['imageUrl']        = $lightboxPicture->getImg($projectDir, $staticUrl)['src'];
                 }
-            } catch (\Throwable $exception) {
+            } catch (Throwable $exception) {
                 // Unreadable broken image - ignore.
                 $information['isPicture'] = false;
             }
