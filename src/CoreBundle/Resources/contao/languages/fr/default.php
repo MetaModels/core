5a81ed509ae08db0839dbedb213b5afa9c6abbb7
Mini Model <minimodel@metamodel.me>
2025-02-24 17:51:20 +0100

    Updated translations from transifex.

--- src/CoreBundle/Resources/contao/languages/fr/default.php
+++ src/CoreBundle/Resources/contao/languages/fr/default.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2024 The MetaModels team.
+ * (c) 2012-2025 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,14 +13,18 @@
  * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * Last-updated: 2024-07-10T13:37:16+00:00
+ * Last-updated: 2025-02-24T16:47:24+00:00
  *
- * @copyright 2012-2024 The MetaModels team.
+ * @copyright 2012-2025 The MetaModels team.
  * @license   https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @link      https://www.transifex.com/metamodels/public/
  * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['0']           = 'Liste MetaModels';
 $GLOBALS['TL_LANG']['CTE']['metamodel_content']['1']           = 'Ajoute une liste d\'éléments MetaModel dans l\'article.';
+$GLOBALS['TL_LANG']['CTE']['metamodels']                       = 'Éléments MetaModels';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendclearall']['0'] = 'Effacer tous les MetaModels';
 $GLOBALS['TL_LANG']['CTE']['metamodels_frontendclearall']['1'] = 'Ajoute un "Effacer tout" pour tous les filtres du font office.';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['0']   = 'Filtre frontend MetaModels';
 $GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['1']   = 'Ajoute un frontend filtre pour un MetaModel.';
