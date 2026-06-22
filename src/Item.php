212596723cd61a0e8dd17a969bee74598f69bf4c
Ingolf Steinhardt <info@e-spin.de>
2026-06-22 16:39:59 +0200

    Fix code style

--- src/Item.php
+++ src/Item.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/core.
  *
- * (c) 2012-2024 The MetaModels team.
+ * (c) 2012-2026 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -19,7 +19,7 @@
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2024 The MetaModels team.
+ * @copyright  2012-2026 The MetaModels team.
  * @license    https://github.com/MetaModels/core/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -614,7 +614,7 @@ class Item implements IItem, IDirtyTracking
         // value currently in the data array, which may be fallback-language data of the source item.
         foreach (\array_keys($arrNewData) as $strColName) {
             $attribute = $metaModel->getAttribute($strColName);
-            if (null === $attribute || $attribute instanceof ITranslated) {
+            if (!$attribute instanceof ITranslated) {
                 continue;
             }
             $objCopy->dirtyAttributes[$strColName] = true;
